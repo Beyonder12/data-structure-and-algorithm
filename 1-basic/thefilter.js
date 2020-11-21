7ff685b8f733e5b2b980e4cabc33af0c102b3ef3
Fajr <fajri.illahi@medicaboo.com>
2020-11-21 18:18:03 +0700

    Update thefilter.js

--- 1-basic/thefilter.js
+++ 1-basic/thefilter.js
@@ -1,3 +1,12 @@
 function getCount(objects) {
-    return objects.filter(function(o){return o.x==o.y}).length
+  return objects.filter((item) => {return item.x == item.y}).length
 }
+
+let ag = [
+  {x: 1, y: 1},
+  {x: 1, y: 1},
+  {x: 1, y: 1},
+  {x: 1, y: 2},
+]
+
+console.log(getCount(ag))
