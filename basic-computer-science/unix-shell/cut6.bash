d3ab54797eb3d59c212630fd6a2287dd1c7eb63e
Fajr <fajri.illahi@medicaboo.com>
2020-11-29 08:06:58 +0700

    Create cut6.bash

---
+++ basic-computer-science/unix-shell/cut6.bash
@@ -0,0 +1,23 @@
+: '
+
+New York is a state in the Northeastern and Mid-Atlantic regions of the United States.
+New York is the 27th-most extensive, the third-most populous populated of the 50 United States.
+New York is bordered by New Jersey and Pennsylvania to the south.
+About one third of all the battles of the Revolutionary War took place in New York.
+Henry Hudsons 1609 voyage marked the beginning of European involvement with the area.
+
+a state in the Northeastern and Mid-Atlantic regions of the United States.
+the 27th-most extensive, the third-most populous populated of the 50 United States.
+bordered by New Jersey and Pennsylvania to the south.
+ird of all the battles of the Revolutionary War took place in New York.
+s 1609 voyage marked the beginning of European involvement with the area.
+
+
+'
+
+#!/usr/bin/env #!/usr/bin/env bash
+
+IFS=""
+while read line; do
+  echo -e "$line" | cut -f-3
+done
