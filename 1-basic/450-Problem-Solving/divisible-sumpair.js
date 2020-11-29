e2d73e95568c44fe40a610dc65f34afb76b2cadd
Fajr <fajri.illahi@medicaboo.com>
2020-11-29 08:06:36 +0700

    Create divisible-sumpair.js

---
+++ 1-basic/450-Problem-Solving/divisible-sumpair.js
@@ -0,0 +1,15 @@
+
+
+function divisibleSumPairs(n, k, ar) {
+  var count = 0;
+  for(let i = 0; i < n; i++) {
+    for(let j = (i+1); j < n; j++) {
+      if((ar[i] + ar[j]) % k === 0) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+console.log(divisibleSumPairs(6, 3, [1,3,2,6,1,2]))
