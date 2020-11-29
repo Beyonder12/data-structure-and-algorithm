22e6278eb29d53a2b9394fed0206b101f3c0a4b9
Fajr <fajri.illahi@medicaboo.com>
2020-11-29 08:08:03 +0700

    Create concatenatearrwithistself.bash

---
+++ basic-computer-science/unix-shell/concatenatearrwithistself.bash
@@ -0,0 +1,18 @@
+
+# Namibia
+# Nauru
+# Nepal
+# Netherlands
+# NewZealand
+# Nicaragua
+# Niger
+# Nigeria
+# NorthKorea
+# Norway
+
+
+# Namibia Nauru Nepal Netherlands NewZealand Nicaragua Niger Nigeria NorthKorea Norway Namibia Nauru Nepal Netherlands NewZealand Nicaragua Niger Nigeria NorthKorea Norway Namibia Nauru Nepal Netherlands NewZealand Nicaragua Niger Nigeria NorthKorea Norway
+
+array=($(cat))
+total=("${array[@]}" "${array[@]}" "${array[@]}")
+echo ${total[@]}
