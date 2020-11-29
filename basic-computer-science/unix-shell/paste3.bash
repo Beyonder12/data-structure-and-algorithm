4e10b178b6b7be7374ef5759cef06f0b080600d5
Fajr <fajri.illahi@medicaboo.com>
2020-11-29 08:07:21 +0700

    Create paste3.bash

---
+++ basic-computer-science/unix-shell/paste3.bash
@@ -0,0 +1,23 @@
+:
+'Albany, N.Y.
+Albuquerque, N.M.
+Anchorage, Alaska
+Asheville, N.C.
+Atlanta, Ga.
+Atlantic City, N.J.
+Austin, Texas
+Baltimore, Md.
+Baton Rouge, La.
+Billings, Mont.
+Birmingham, Ala.
+Bismarck, N.D.
+Boise, Idaho
+Boston, Mass.
+Bridgeport, Conn.
+
+
+
+Albany, N.Y.    Albuquerque, N.M.   Anchorage, Alaska   Asheville, N.C.Atlanta, Ga. Atlantic City, N.J. Austin, Texas   Baltimore, Md.  Baton Rouge, La.    Billings, Mont. Birmingham, Ala.    Bismarck, N.D.  Boise, Idaho    Boston, Mass.   Bridgeport, Conn.'
+
+
+tr '\n' '\t'
