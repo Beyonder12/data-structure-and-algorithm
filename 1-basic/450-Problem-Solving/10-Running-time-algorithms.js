2db60f1fe82e855fd3c316292d52fd2b18f93343
Fajr <fajri.illahi@medicaboo.com>
2020-12-03 09:03:14 +0700

    Create 10-Running-time-algorithms.js

---
+++ 1-basic/450-Problem-Solving/10-Running-time-algorithms.js
@@ -0,0 +1,14 @@
+function runningTime(arr) {
+    let count = 0;
+    for (let i = 1; i < arr.length; i++) {
+        let temp = arr[i];
+        for (let j = i - 1; j >=0; j--) {
+            if (temp < arr[j]) {
+                count++;
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return count;
+}
