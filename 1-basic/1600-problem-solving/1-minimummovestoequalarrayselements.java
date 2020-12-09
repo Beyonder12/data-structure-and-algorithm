fd2712b74187c2a9b3a3724a8388b8aa11d264a7
Fajr <fajri.illahi@medicaboo.com>
2020-12-09 09:20:09 +0700

    Update 1-minimummovestoequalarrayselements.java

--- 1-basic/1600-problem-solving/1-minimummovestoequalarrayselements.java
+++ 1-basic/1600-problem-solving/1-minimummovestoequalarrayselements.java
@@ -1,6 +1,26 @@
+
+// Ideas
+// sum + (n - 1) * move = target * n
+// target = min + move
+// sum - move = min * n
+class minMoves {
+
+    public int minMoves(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+        for(int num : nums) {
+            min = Math.min(min, num);
+            sum += num;
+        }
+    
+    }
+}
+
+
+/*
 class Solution {
     public int minMoves(int[] nums) {
-        of (nums == null || nums.length <= 1) {
+        if (nums == null || nums.length <= 1) {
             return 0;
         }
 
@@ -13,3 +33,4 @@ class Solution {
         return (int)(sum - nums.length * min);
     }
 }
+*/
