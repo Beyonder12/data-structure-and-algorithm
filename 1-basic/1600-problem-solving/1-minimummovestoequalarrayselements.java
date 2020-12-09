f5b784f415336cfdc016fd0398766538be0b5711
Fajr <fajri.illahi@medicaboo.com>
2020-12-09 09:07:11 +0700

    Create 1-minimummovestoequalarrayselements.java

---
+++ 1-basic/1600-problem-solving/1-minimummovestoequalarrayselements.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minMoves(int[] nums) {
+        of (nums == null || nums.length <= 1) {
+            return 0;
+        }
+
+        int min = nums[0];
+        long sum = 0;
+        for (int num : nums) {
+            sum += num;
+            min = Math.min(min, num);
+        }
+        return (int)(sum - nums.length * min);
+    }
+}
