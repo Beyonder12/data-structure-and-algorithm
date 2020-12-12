29f383ff04c293a06687d3aa5fb131bafa716c5c
Fajr <fajri.illahi@medicaboo.com>
2020-12-13 02:06:19 +0700

    Create Solution.java

---
+++ 1-basic/basic/11-javasubstring/Solution.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print( in.next().substring( in.nextInt(), in.nextInt() ) );
+    }
+}
+
+/*
+
+Helloworld
+3
+7
+lowo% 
+*/
