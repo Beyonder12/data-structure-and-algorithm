fe489d20ca56c1f43a4a8da1cb1715cd7958166d
Fajr <fajri.illahi@medicaboo.com>
2020-12-13 02:06:42 +0700

    Create 1-maxnumbersksumpairs.js

---
+++ 2-intermediate/CP/1-maxnumbersksumpairs.js
@@ -0,0 +1,9 @@
+var temp = "This is a string. is";
+
+var count = (temp.split('is').length - 1);
+
+console.log(count);
+words = ["ad","bd","aaab","baa","badab"]
+allowed = "abc"
+let a = words.filter(word => word.split('').includes(allowed).length)
+
