c9f8cf5d713168e02af2f07fd3e2389e892151c5
Fajr <fajri.illahi@medicaboo.com>
2020-12-12 07:52:02 +0700

    Create Solution.java

---
+++ 1-basic/basic/5-javaendoflife/Solution.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    //while loop
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = 0; 
+        while(scan.hashNext()){
+            i++;
+            System.out.println(i + " " + scan.nextLine());
+        }
+    }
+}
