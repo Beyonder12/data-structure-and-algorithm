dbf6b2a32824789146214e3f31f4ac22a0528827
Fajr <fajri.illahi@medicaboo.com>
2020-12-13 02:06:13 +0700

    Create Solution.java

---
+++ 1-basic/basic/18-regexduplicatewords/Solution.java
@@ -0,0 +1,50 @@
+/*
+
+5
+Goodbye bye bye world world world
+Sam went went to to to his business
+Reya is is the the best player in eye eye game
+in inthe
+Hello hello Ab aB
+
+
+Goodbye bye world
+Sam went to his business
+Reya is the best player in eye game
+in inthe
+Hello Ab
+*/
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        String regex = "\\b(\\w+)(?:\\W+\\1\\b)+";
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+
+        Scanner in = new Scanner(System.in);
+        int numSentences = Integer.parseInt(in.nextLine());
+
+        while (numSentences-- > 0) {
+            String input = in.nextLine();
+
+            Matcher m = p.matcher(input);
+
+            // Check for subsquence of input that match the compiled pattern
+
+            while (m.find()) {
+                input = input.replaceAll(m.group(), m.group(1));
+            }
+
+            // Prints the modified sentence
+            System.out.println(input);
+        }
+        in.close();
+    }
+    
+}
