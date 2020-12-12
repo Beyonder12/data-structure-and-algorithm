1e7b08eab7d403c307de6bd101d304caaba8c6bb
Fajr <fajri.illahi@medicaboo.com>
2020-12-13 02:06:09 +0700

    Create Solution.java

---
+++ 1-basic/basic/14-anagramjava/Solution.java
@@ -0,0 +1,42 @@
+/*
+input :
+Hello
+hello
+
+output:
+Anagrams
+*/
+
+import java.util.Scanner;
+//public access modifier used for classes, attributes, methods, and constructors
+// making them accessible by any other class.
+
+// other than that, private-----making them only accessible within the declared class
+public class Solution {
+    
+    static boolean isAnagram(String a, String b) {
+        char aa[] = a.toLowerCase().toCharArray();
+        char bb[] = b.toLowerCase().toCharArray();
+        
+        if(a.length() != b.length()) {
+            return false;
+        }else{
+            java.util.Arrays.sort(aa);
+            java.util.Arrays.sort(bb);
+            return java.util.Arrays.equals(aa,bb);
+            
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println(aa);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams");
+    }
+
+}
