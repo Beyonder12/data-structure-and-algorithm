7ffcb10d3faf5df72785e9d932f22fc2cf381c60
Fajr <fajri.illahi@medicaboo.com>
2020-12-13 02:06:29 +0700

    Create Solution.java

---
+++ 1-basic/basic/13-javastringreverse/Solution.java
@@ -0,0 +1,30 @@
+
+//palindrome reverse
+
+/*
+madam
+yes
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        int lengthOfA = A.length()-1;
+        String answer = "Yes";
+        for(int i = 0; i<lengthOfA/2; i++) {
+            if(A.charAt(i) == A.charAt(lengthOfA-i))
+                continue;
+            else {
+                answer = "No";
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+    
+}
