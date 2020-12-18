275a604ddc522425b58d03d5924ac1bae59ae979
Fajr <fajri.illahi@medicaboo.com>
2020-12-19 05:18:59 +0700

    Create dictionariemap.js

---
+++ 1-basic/30days/8-dictionariesandmap/dictionariemap.js
@@ -0,0 +1,33 @@
+function processData(input) {
+    input = input.split('\n')
+    var N = parseInt(input[0])
+
+    var phonebook = []
+    for (var k = 0; k< N; k++){
+        var line = input[k+1]
+        line = line.split(' ')
+        phonebook[line[0]] = line[1]
+    }
+
+    for (var k = N+1; k < input.length; k++ ){
+        var num = (phonebook[input[k]])
+        if (num !== undefined){
+            console.log(input[k]+'='+num)
+        } else {
+            console.log('Not found')
+        }
+    }
+}
+
+
+// IGNORE
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+processData(_input);    
+});
