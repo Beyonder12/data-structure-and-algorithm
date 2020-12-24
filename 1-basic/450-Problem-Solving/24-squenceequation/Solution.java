7ea8a2ceaf156af8302a9830b1ecca35b1c13dde
Fajr <fajri.illahi@medicaboo.com>
2020-12-24 07:44:42 +0700

    Create Solution.java

---
+++ 1-basic/450-Problem-Solving/24-squenceequation/Solution.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] permutationEquation(int[] p) {
+
+        int[] result = new int[p.length];
+
+        int[] indexArr = new int[p.length];
+
+        for (int i = 0; i < p.length; i++) {
+            int val = p[i];
+            indexArr[val - 1] = i +1;
+        }
+        for (int i =0; i < p.length; i++) {
+            result[i] = indexArr[indexArr[i] - 1];
+        }
+        return result;
+    }
+
+    private static final Scanner scanner =  new Scanner (System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] p = new int[n];
+
+        String[] pItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int pItem = Integer.parseInt(pItems[i]);
+            p[i] = pItem;
+        }
+
+        int[] result = permutationEquation(p);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+ 
+
