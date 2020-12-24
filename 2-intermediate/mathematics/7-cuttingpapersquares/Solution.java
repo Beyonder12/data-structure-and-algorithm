c7f41be00bdcbc2c71e758f2972948d2e8a422aa
Fajr <fajri.illahi@medicaboo.com>
2020-12-24 07:45:10 +0700

    Create Solution.java

---
+++ 2-intermediate/mathematics/7-cuttingpapersquares/Solution.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println((sc.nextLong())*(sc.nextLong())-1);
+    }
+}
