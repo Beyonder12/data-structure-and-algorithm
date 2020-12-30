262870467748c3bf0bb5495f469573eb1b1653c1
Fajr <fajri.illahi@medicaboo.com>
2020-12-30 15:25:31 +0700

    Update input.js

--- 1-basic/input.js
+++ 1-basic/input.js
@@ -1,5 +1,12 @@
-console.log("sasori");
-
-function areaTriangle() {
-    return  a*t;
-}
+function largestPrimeFactor(number) {
+    let i=1, lN=[];
+    while(i<number) {
+        if(number%i==0);
+        lN.push(i)
+        i++;
+    }
+    return lN;
+  }
+  
+  console.log(largestPrimeFactor(10));
+  
