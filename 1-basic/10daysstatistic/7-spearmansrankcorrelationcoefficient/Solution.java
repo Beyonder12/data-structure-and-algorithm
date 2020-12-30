d8dac484b61bd3156fbba6f0bffe914ebc8bbe91
Fajr <fajri.illahi@medicaboo.com>
2020-12-30 15:25:41 +0700

    Create Solution.java

---
+++ 1-basic/10daysstatistic/7-spearmansrankcorrelationcoefficient/Solution.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
+
+/*
+input:
+
+10
+10 9.8 8 7.8 7.7 1.7 6 5 1.4 2 
+200 44 32 24 22 17 15 12 8 4
+
+output:
+0.903
+
+
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        double [] X = new double[size];
+        double [] Y = new double[size];
+        for (int i = 0; i < size; i++) {
+            X[i] = scan.nextDouble();
+        }
+        for (int i = 0; i < size; i++) {
+            Y[i] = scan.nextDouble();
+        }
+        scan.close();
+
+        System.out.format("%.3f", spearman(X, Y));
+    }
+
+    private static Double spearman(double [] X, double [] Y) {
+        if (X == null || Y == null || X.length != Y.length) {
+            return null;
+        }
+
+        int [] rankX = getRanks(X);
+        int [] rankY = getRanks(Y);
+
+        int n = X.length;
+        double numerator = 0;
+        for  (int i = 0; i < n; i++) {
+            numerator += Math.pow((rankX[i] - rankY[i]),2);
+        }
+        numerator *=6;
+        return 1 - numerator / (n * ((n * n) - 1));
+    }
+
+    public static int[] getRanks(double [] array) {
+        int n = array.length;
+
+        Pair[] pair = new Pair[n];
+        for (int i = 0; i < n; i++) {
+            pair[i] = new Pair(i, array[i]);
+        }
+        Arrays.sort(pair, new PairValueComparator());
+
+        int [] ranks = new int[n];
+        int rank = 1;
+        for (Pair p : pair) {
+            ranks[p.index] = rank++;
+        }
+        return ranks;
+    }
+    
+}
+
+class Pair {
+    public final int index;
+    public final double value;
+
+    public Pair(int i, double v) {
+        index = i;
+        value = v;
+    }
+}
+
+class PairValueComparator implements Comparator <Pair> {
+    double epsilon = 0.0001;
+    @Override
+    public int compare(Pair p1, Pair p2) {
+        if (Math.abs(p1.value - p2.value) < epsilon) {
+            return 0;
+        } else if (p1.value < p2.value) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+}
