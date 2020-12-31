6d746f8a72e3a56442dc279130bbf181fb64a51e
Fajr <fajri.illahi@medicaboo.com>
2020-12-31 07:27:33 +0700

    Update input.js

--- 1-basic/input.js
+++ 1-basic/input.js
@@ -1,12 +1,1 @@
-function largestPrimeFactor(number) {
-    let i=1, lN=[];
-    while(i<number) {
-        if(number%i==0);
-        lN.push(i)
-        i++;
-    }
-    return lN;
-  }
-  
-  console.log(largestPrimeFactor(10));
-  
+console.log(2784378948059382..toString(36))
