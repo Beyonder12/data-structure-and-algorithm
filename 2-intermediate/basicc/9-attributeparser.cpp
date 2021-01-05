43ff7f483dd939dc528a9de04a6fa24160e55e74
Fajr <fajri.illahi@medicaboo.com>
2021-01-05 09:03:44 +0700

    Update 9-attributeparser.cpp

---
+++ 2-intermediate/basicc/9-attributeparser.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+/*
+header map : construct a map container object, initializing its
+contents depending on the constructor version used.
+map no two mapped values can have the same key values.
+*/
+
+
+map <string, string> tagMap;
+
+void createMap(int &n, string pretag) {
+    if(!n) return;
+
+    string line, tag, attr, value;
+    getline(cin, line);
+
+    int i=1;
+    if(line[i]=='/') {
+        while(line[i]!='>') i++;
+        if(pretag.size()>(i-2))
+            tag = pretag.substr(0,pretag.size()-i+1);
+        else
+            tag = "";
+    }
+    else {
+        while(line[i]!=' ' && line[i]!='>') i++;
+        tag = line.substr(1, i-1);
+        if(pretag!="") tag = pretag + "." + tag;
+
+        int j;
+        while(line[i]!='>') {
+            j = ++i;
+            while(line[i]!=' ') i++;
+            attr = line.substr(j, i-j);
+
+            while(line[i]!='\"') i++;
+            j = ++i;
+            while(line[i]!='\"') i++;
+            value = line.substr(j, i-j);
+            i+= 1;
+
+            tagMap[tag + "~" + attr] = value;
+        }
+    }
+    createMap(--n, tag);
+}
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    cin.ignore();
+    createMap(n,"");
+
+    string attr, value;
+    while(q--) {
+        getline(cin,attr);
+        value = tagMap[attr];
+        if(value=="") value = "Not Found!";
+        cout << value << endl;
+    }
+    return 0;
+}
