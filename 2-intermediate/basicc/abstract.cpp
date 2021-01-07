dbe43d6416780a890d0d2f2777e37e961ee460e6
Fajr <fajri.illahi@medicaboo.com>
2021-01-07 09:01:38 +0700

    Create abstract.cpp

---
+++ 2-intermediate/basicc/abstract.cpp
@@ -0,0 +1,17 @@
+// invalid_argument example
+#include <iostream>       // std::cerr
+#include <stdexcept>      // std::invalid_argument
+#include <bitset>         // std::bitset
+#include <string>         // std::string
+
+int main (void) {
+  try {
+    // bitset constructor throws an invalid_argument if initialized
+    // with a string containing characters other than 0 and 1
+    std::bitset<5> mybitset (std::string("01234"));
+  }
+  catch (const std::invalid_argument& ia) {
+	  std::cerr << "Invalid argument: " << ia.what() << '\n';
+  }
+  return 0;
+}
