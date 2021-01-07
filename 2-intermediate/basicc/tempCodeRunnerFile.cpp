62617ba74abde122524c9ecf2584ded7803a0e5e
Fajr <fajri.illahi@medicaboo.com>
2021-01-08 06:06:29 +0700

    Update tempCodeRunnerFile.cpp

--- 2-intermediate/basicc/tempCodeRunnerFile.cpp
+++ 2-intermediate/basicc/tempCodeRunnerFile.cpp
@@ -1,53 +1,151 @@
 #include <iostream>
+#include <vector>
 #include <string>
-//construct a stringstream object (stream class to operate on strings)
-// object of this class use a string buffer that contains a squence of characters
-#include <sstream>
-#include <exception>
 using namespace std;
 
-class BadLengthException {
-    private:
-        int n;
-    public:
-        BadLengthException(int errornumber) {
-            n = errornumber;
-        }
+class Spell
+{
+private:
+    string scrollName;
 
-        int what() {
-            return n;
-        }
+public:
+    Spell() : scrollName("") {}
+    Spell(string name) : scrollName(name) {}
+    virtual ~Spell() {}
+    string revealScrollName()
+    {
+        return scrollName;
+    }
 };
 
-bool checkUsername(string username) {
-    bool isValid = true;
-    int n = username.length();
-    if(n<5) {
-        throw BadLengthException();
+class Fireball : public Spell
+{
+private:
+    int power;
+
+public:
+    Fireball(int power) : power(power) {}
+    void revealFirepower()
+    {
+        cout << "Fireball: " << power << endl;
     }
-    for(int i = 0; i < n-1; i++) {
-        if(username[i] == 'w' && username[i+1] == 'w') {
-            isValid = false;
+};
+
+class Frostbite : public Spell
+{
+private:
+    int power;
+
+public:
+    Frosbite(int power) : power(power)  {}
+    void revealThunderpower()
+    {
+        cout << "Frosbite: " << power << endl;
+    }
+};
+
+class Thunderstorm : public Spell
+{
+private:
+    int power;
+
+public:
+    Thunderstorm(int power) : power(power) {}
+    void revealThunderpower()
+    {
+        cout << "Thunderstorm: " << power << endl;
+    }
+};
+
+class Waterbolt : public Spell
+{
+private:
+    int power;
+
+public:
+    Waterbolt(int power) : power(power) {}
+    void revealWaterpower()
+    {
+        cout << "Waterbolt: " << power << endl;
+    }
+};
+
+class SpellJournal
+{
+public:
+    static string journal;
+    static string read()
+    {
+        return journal;
+    }
+};
+
+string SpellJournal::journal = "";
+
+void counterspell(Spell *spell)
+{
+    if (dynamic_cast<Fireball *>(spell) != NULL)
+        dynamic_cast<Fireball *>(spell)->revealFirepower();
+    else if (dynamic_cast<Frostbite*>(spell) != NULL)
+        dynamic_cast<Frostbite *>(spell)->revealFrostpower();
+    else if (dynamic_cast<Thunderstorm *>(spell) != NULL)
+        dynamic_cast<Thunderstorm *>(spell)->revealThunderpower();
+    else if (dynamic_cast<Waterbolt *>(spell) != NULL)
+        dynamic_cast<Waterbolt*>(spell)->revealWaterpower();
+    else
+    {
+        std::string str1 = spell->revealScrollName();
+        std::string str2 = SpellJournal::read();
+        int m = str1.length();
+        int n = str2.length();
+        std::vector< std::vector<int> > T(m + 1, std::vector<int>(n + 1, 0));
+        for (int i = 0; i < m + 1; i++)
+        {
+            for (int j = 0; j < n + 1; j++)
+            {
+                if (int j == 0 || i == 0)
+                    T[i][j] = 0;
+                else if (str1[i - 1] == str2[j - 1])
+                    T[i][j] += T[i - 1][j - 1] + 1;
+                else
+                    T[i][j] = std::max(T[i - 1][j], T[i][j - 1]);
+            }
         }
+        std::cout << T[m][n] << std::endl;
     }
-    return isValid;
 }
 
-int main() {
-    int T; cin >> T;
-    while(T--) {
-        string username;
-        cin >> username;
-        try {
-            bool isValid = checkUsername(username);
-            if(isValid) {
-                cout << "Valid" << '\n';
-            } else {
-                cout << "Invalid" << '\n';
-            }
-        } catch (BadLengthException e) {
-            cout << "Too short: " << e.what() << '\n';
+class Wizar
+{
+public:
+    Spell *cast()
+    {
+        Spell *spell;
+        string s;
+        cin >> s;
+        int power;
+        cin >> power;
+        if (s == "fire")
+        {
+            spell = new Fireball(power);
         }
+        else if (s == "frost")
+        {
+            spell = new Frosbite(power);
+        }
+        else if (s == "water")
+        {
+            spell = new Waterbolt(power);
+        }
+        else if (s == "thunder")
+        {
+            spell = new Thunderstorm(power);
+        }
+        else
+        {
+            spell = new Spell(s);
+            cin >> SpellJournal::journal;
+        }
+        return spell;
     }
-    return 0;
-}
+};
