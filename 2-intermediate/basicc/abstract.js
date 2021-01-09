f8081dd0c5b66b54a8d8af483a87c048cc3a2816
Fajr <fajri.illahi@medicaboo.com>
2021-01-09 21:29:13 +0700

    Update abstract.js

--- 2-intermediate/basicc/abstract.js
+++ 2-intermediate/basicc/abstract.js
@@ -1,21 +1,18 @@
-function modus(nums) {
-  var a = [];
-  nums = nums.sort()
-  for (let i = 0; i < nums.length; i++) {
-
-    let c = 1;
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] == nums[j]) {
-        c++;
-      }
+var majorityElement = function(nums) {
+  let obj = {};
+  let best;
+  let temp = -Infinity;
+  
+  nums.forEach((elem)=> {
+     obj[elem] = ++obj[elem] || 1; 
+  })
+  
+  for (let key in obj){
+    temp = Math.max(temp, obj[key]);
+    if(obj[key] >= temp){
+       best = key; 
     }
-    a.push(c);
   }
-  return a;
-
-}
-
-console.log(modus([2, 2, 1, 1, 1, 2, 2]))
-x = [2, 2, 3, 1]
-x = x.sort()
-console.log(x)
+  return best;
+    
+};
