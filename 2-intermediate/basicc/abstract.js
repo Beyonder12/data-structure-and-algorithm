7f869b3840be33932681c62c2c366226a144d032
Fajr <fajri.illahi@medicaboo.com>
2021-01-10 23:08:16 +0700

    Update abstract.js

--- 2-intermediate/basicc/abstract.js
+++ 2-intermediate/basicc/abstract.js
@@ -1,18 +1,20 @@
-var majorityElement = function(nums) {
+var majorityElement = function (nums) {
   let obj = {};
   let best;
-  let temp = -Infinity;
+  let temp = -Infinity; 
   
-  nums.forEach((elem)=> {
-     obj[elem] = ++obj[elem] || 1; 
+  nums.forEach((elem) => {
+    obj[elem] = ++obj[elem] || 1;
   })
-  
-  for (let key in obj){
+
+  for (let key in obj) {
     temp = Math.max(temp, obj[key]);
-    if(obj[key] >= temp){
-       best = key; 
+    if (obj[key] >= temp) {
+      best = key;
     }
   }
   return best;
-    
+
 };
+console.log(majorityElement([2,1, 2, 0, 2, 3, 3, 3]))
+console.log(Math.max([1,8, 9, 2]));
