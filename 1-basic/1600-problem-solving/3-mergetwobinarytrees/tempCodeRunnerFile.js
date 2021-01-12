a8f57056fec76a20e89e33d6011f83e14e6582d0
Fajr <fajri.illahi@medicaboo.com>
2021-01-12 17:02:32 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/1600-problem-solving/3-mergetwobinarytrees/tempCodeRunnerFile.js
+++ 1-basic/1600-problem-solving/3-mergetwobinarytrees/tempCodeRunnerFile.js
@@ -1,1 +1,26 @@
-[1,3,2,5],[2,1,3,null,4,null,7]
+function Stack() {
+    var collection = [];
+    this.print = function() {
+      console.log(collection);
+    };
+    this.push = function(val) {
+      return collection.push(val);
+    };
+    this.pop = function() {
+      return collection.pop();
+    };
+    this.peek = function() {
+      return collection[collection.length - 1];
+    };
+    this.isEmpty = function() {
+      return collection.length === 0;
+    };
+    this.clear = function() {
+      collection.length = 0;
+    };
+  }
+  
+  var stack = new Stack();
+  stack.push(4);
+  stack.push(5);
+  console.log(stack.clear());
