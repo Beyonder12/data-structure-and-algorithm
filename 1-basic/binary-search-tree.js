e3e3a71914e65187ff258bda4b96a6e48afb10e2
Fajr <fajri.illahi@medicaboo.com>
2021-01-14 09:52:49 +0700

    Update binary-search-tree.js

--- 1-basic/binary-search-tree.js
+++ 1-basic/binary-search-tree.js
@@ -122,6 +122,12 @@ class BST {
             return right + 1;
         };
     }
+
+    maxDepth = function(root) {
+        if (!root) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    };
+
     findMaxHeight(node = this.root) {
         if (node == null) {
             return -1;
