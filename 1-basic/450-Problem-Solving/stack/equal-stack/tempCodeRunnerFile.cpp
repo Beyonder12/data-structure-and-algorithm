c5fac746e13e606294ee41a67a73a6a5c084a005
Fajr <fajri.illahi@medicaboo.com>
2021-01-15 14:26:07 +0700

    Create tempCodeRunnerFile.cpp

---
+++ 1-basic/450-Problem-Solving/stack/equal-stack/tempCodeRunnerFile.cpp
@@ -0,0 +1,15 @@
+int a[N];
+// int b[N];
+// int c[N];
+
+// int main() {
+//     int n1,n2,n3;
+//     cin>>n1>>n2>>n3;
+//     for(int  i = 1; i <= n1; i++)cin>>a[i];
+//     for(int  i = 1; i <= n2; i++)cin>>b[i];
+//     for(int  i = 1; i <= n3; i++)cin>>c[i];
+
+//     reverse(a + 1, a + n1 + 1);
+//     reverse(b + 1, b + n2 + 1);
+//     reverse(c + 1, c + n3 + 1);
+// }
