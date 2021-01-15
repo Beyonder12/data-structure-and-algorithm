27991eb97b7195b328b63bfa80743be7a6f8707f
Fajr <fajri.illahi@medicaboo.com>
2021-01-15 14:25:44 +0700

    Update input.js

--- 1-basic/input.js
+++ 1-basic/input.js
@@ -1,1 +1,14 @@
-console.log(2784378948059382..toString(36))
+var reverseString = function (s) {
+    let i = 0,
+      j = s.length - 1;
+    while (i < j) {
+      const temp = s[j];
+      s[j] = s[i];
+      s[i] = temp;
+      i++;
+      j--;
+    }
+    return s;
+  };
+
+  console.log(reverseString([1,2,3,4,5]));
