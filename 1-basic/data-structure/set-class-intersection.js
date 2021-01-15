7b2489a87b0244a3e68ade196ed7c103eeedff3a
Fajr <fajri.illahi@medicaboo.com>
2021-01-15 14:25:54 +0700

    Create set-class-intersection.js

---
+++ 1-basic/data-structure/set-class-intersection.js
@@ -0,0 +1,68 @@
+class Set {
+    constructor() {
+        this.dictionary = {};
+        this.length = 0;
+    }
+
+    has(element) {
+        return this.dictionary[element] !== undefined;
+    }
+
+    values() {
+        return Object.keys(this.dictionary);
+    }
+
+    add(element) {
+        if (!this.has(element)) {
+            this.dictionary[element] = true;
+            this.length++;
+            return true;
+        }
+        return false;
+    }
+
+    remove(element) {
+        if (this.has(element)) {
+            delete this.dictionary[element];
+            this.length--;
+            return true;
+        }
+
+        return false;
+    }
+
+    size() {
+        return this.length;
+    }
+
+    union(set) {
+        const newSet = new Set();
+        this.values().forEach(value => {
+            newSet.add(value);
+        })
+
+        return newSet;
+    }
+
+    intersection(set) {
+        const newSet = new Set();
+
+        let largeSet;
+        let smallSet;
+        if (this.dictionary.length > set.length) {
+            largeSet = this;
+            smallSet = set;
+        } else {
+            largeSet = set;
+            smallSet = this;
+        }
+
+        smallSet.values().forEach(value => {
+            if (largeSet.dictionary[value]) {
+                newSet.add(value);
+            }
+        })
+
+        return newSet;
+    }
+}
