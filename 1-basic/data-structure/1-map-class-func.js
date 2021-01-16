085ea64ac31c20e85031a515a20bc988bf293242
Fajr <fajri.illahi@medicaboo.com>
2021-01-17 06:29:26 +0700

    Create 1-map-class-func.js

---
+++ 1-basic/data-structure/1-map-class-func.js
@@ -0,0 +1,42 @@
+var Map = function() {
+    this.collection = {};
+
+    this.add = (key,value) => {
+        this.collection[key] = value;
+        return this.collection[key];
+    };
+
+    this.remove = (key)=>{
+        if(this.collection.hasOwnProperty(key)){
+            this.collection.delete(key);
+        }
+    };
+
+    this.get = (key)=>{
+        return this.collection[key];
+    }
+
+    this.has = (item)=>{
+        return (this.collection.hasOwnProperty(item));
+    };
+
+    this.values = ()=>{
+        let ar = [];
+        for(let v in this.collection) {
+            ar.push(this.collection[v]);
+        }
+        return ar;
+    };
+    this.size = ()=>(Object.keys(this.collection).length);
+    this.clear = ()=>{
+        this.collection = {};
+    };
+};
+
+var m1 = new Map();
+m1.add("p1","Tom");
+m1.add("p2","Jerry");
+m1.add("p3", "Julie");
+console.log(m1.values());
+console.log(m1.size());
+m1.clear();
