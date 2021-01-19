b681bd2040130c7cb119532086b21c37bb68bdd2
Fajr <fajri.illahi@medicaboo.com>
2021-01-19 17:12:48 +0700

    Create 5-linkedlist-class-func-remove.js

---
+++ 1-basic/data-structure/5-linkedlist-class-func-remove.js
@@ -0,0 +1,52 @@
+function LinkedList() {
+    var length = 0;
+    var head = null;
+
+    var Node = function(element) {
+        this.element = element;
+        this.next = null;
+    };
+
+    this.size = function(){
+        return length;
+    };
+
+    this.head = function(){
+        return head;
+    };
+
+    this.add = function(element) {
+        var node = new Node(element);
+        if(head == null){
+            head = node;
+        } else {
+            let current = head;
+
+            while(current.next){
+                current = current.next;
+            }
+            current.next = node;
+        }
+        length++;
+    };
+
+    this.remove = function (element) {
+
+        if (head.element === element) {
+            head = head.next;
+            return length--;
+        }
+        let previous = head;
+        while (previous) {
+            let current = previous.next;
+            if (current) { // makes sure we are not at end where current.next would be null
+                if (current.element === element) {
+                    previous.next = current.next;
+                    return length--;
+                }
+            }
+            previous = current;
+        }
+
+    };
+}
