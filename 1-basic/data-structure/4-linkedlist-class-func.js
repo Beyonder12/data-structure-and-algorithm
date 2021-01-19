565eb71c79c51b4480558bfc539b5affc19535c4
Fajr <fajri.illahi@medicaboo.com>
2021-01-19 17:12:53 +0700

    Create 4-linkedlist-class-func.js

---
+++ 1-basic/data-structure/4-linkedlist-class-func.js
@@ -0,0 +1,28 @@
+function LinkedList() {
+    let length = 0;
+    let head = null;
+
+    function Node(element) {
+        this.element = element;
+        this.next = null;
+    }
+
+    this.head = () => head;
+
+    this.size = () => length;
+
+    this.add = element => {
+        const node = new Node(element);
+        if (head) {
+            let current = head;
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        else {
+            head = node;
+        }
+        length++;
+    };
+}
