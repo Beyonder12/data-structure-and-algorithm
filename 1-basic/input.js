61e35efd8f4fb2fca889216189d707a6eba60acc
Fajr <fajri.illahi@medicaboo.com>
2021-01-21 11:32:18 +0700

    Update input.js

--- 1-basic/input.js
+++ 1-basic/input.js
@@ -1,164 +1,87 @@
-// class Node {
-//     constructor(data, next = null) {
-//         this.data = data;
-//         this.next = next;
-//     }
-// }
-
-// const n1 = new Node(100);
-// console.log(n1);
-
-// a = null;
-// a = 5
-// console.log(a)
-
-// Construct Single Node
-class Node {
-    constructor(data, next = null) {
-      this.data = data;
-      this.next = next;
+function LinkedList() {
+    var length = 0;
+    var head = null;
+  
+    var Node = function(element){ // {1}
+      this.element = element;
+      this.next = null;
+    };
+  
+    this.size = function() {
+      return length;
+    };
+  
+    this.head = function(){
+      return head;
+    };
+  
+    this.add = function(element){
+      var node = new Node(element);
+      if(head === null){
+        head = node;
+      } else {
+        var currentNode = head;
+  
+        while(currentNode.next){
+          currentNode  = currentNode.next;
+        }
+  
+        currentNode.next = node;
+      }
+  
+      length++;
+    };
+  
+    this.remove = function(element){
+      var currentNode = head;
+      var previousNode;
+      if(currentNode.element === element){
+        head = currentNode.next;
+      } else {
+        while(currentNode.element !== element) {
+          previousNode = currentNode;
+          currentNode = currentNode.next;
+        }
+  
+        previousNode.next = currentNode.next;
+      }
+  
+      length --;
+    };
+  
+    this.isEmpty = function() {
+      return this.size() > 0 ? false : true;
+    };
+  
+    this.indexOf = function(el) {
+      let currentNode = head, index = -1, indexFound = false;
+  
+      while (!indexFound && currentNode) {
+        index++;
+        if(currentNode.element === el) {
+          indexFound = true;
+        }
+        currentNode = currentNode.next;
+      } 
+  
+      return indexFound ? index : -1;
+    };
+  
+    this.elementAt = function(i) {
+      let currentNode = head, currentElement, index = -1, indexReached = false;
+  
+      while (!indexReached && currentNode) {
+        index++;
+        currentElement = currentNode.element;
+        if(index === i) {
+          indexReached = true;
+        }
+        currentNode = currentNode.next;
+      } 
+  
+      return indexReached ? currentElement : undefined;
     }
   }
   
-  // Create/Get/Remove Nodes From Linked List
-  class LinkedList {
-    constructor() {
-      this.head = null;
-      this.size = 0;
-    }
-  
-    // Insert first node
-    insertFirst(data) {
-      this.head = new Node(data, this.head);
-      this.size++;
-    }
-  
-    // Insert last node
-    insertLast(data) {
-      let node = new Node(data);
-      let current;
-  
-      // If empty, make head
-      if (!this.head) {
-        this.head = node;
-      } else {
-        current = this.head;
-  
-        while (current.next) {
-          current = current.next;
-        }
-  
-        current.next = node;
-      }
-  
-      this.size++;
-    }
-  
-    // Insert at index
-    insertAt(data, index) {
-      //  If index is out of range
-      if (index > 0 && index > this.size) {
-        return;
-      }
-  
-      // If first index
-      if (index === 0) {
-        this.insertFirst(data);
-        return;
-      }
-  
-      const node = new Node(data);
-      let current, previous;
-  
-      // Set current to first
-      current = this.head;
-      let count = 0;
-  
-      while (count < index) {
-        previous = current; // Node before index
-        count++;
-        current = current.next; // Node after index
-      }
-  
-      node.next = current;
-      previous.next = node;
-  
-      this.size++;
-    }
-  
-    // Get at index
-    getAt(index) {
-      let current = this.head;
-      let count = 0;
-  
-      while (current) {
-        if (count == index) {
-          console.log(current.data);
-        }
-        count++;
-        current = current.next;
-      }
-  
-      return null;
-    }
-  
-    // Remove at index
-    removeAt(index) {
-      if (index > 0 && index > this.size) {
-        return;
-      }
-  
-      let current = this.head;
-      let previous;
-      let count = 0;
-  
-      // Remove first
-      if (index === 0) {
-        this.head = current.next;
-      } else {
-        while (count < index) {
-          count++;
-          previous = current;
-          current = current.next;
-        }
-  
-        previous.next = current.next;
-      }
-  
-      this.size--;
-    }
-  
-    // Clear list
-    clearList() {
-      this.head = null;
-      this.size = 0;
-    }
-  
-    // Print list data
-    printListData() {
-      let current = this.head;
-  
-      while (current) {
-        console.log(current.data);
-        current = current.next;
-      }
-    }
-  }
-  
-  const ll = new LinkedList();
+  var ll = new LinkedList();
   console.log(ll);
-  
-  ll.insertFirst(100);
-  console.log(ll);
-  ll.insertFirst(200);
-  console.log(ll);
-  ll.insertFirst(300);
-  console.log(ll);
-  ll.insertLast([400,500]);
-  console.log(ll);
-//ll.insertAt(500, 3);
-  
-  // ll.clearList();
-  // ll.getAt(2);
-  
-  ll.printListData();
