66c47488e167baab72822a44f635bb4637ceb456
Fajr <fajri.illahi@medicaboo.com>
2021-01-23 09:17:50 +0700

    Create delete-node-linkedlist.js

---
+++ 1-basic/1600-problem-solving/150-topin/4-delete-node-linkedlist-nt/delete-node-linkedlist.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+var deleteNode = function(node) {
+    [node.val, node.next.val] = [node.next.val, node.val];
+    node.next = node.next.next;
+
+}
