6c9fde8d90cb1b49e1911b77f2b6332168bc928a
Fajr <fajri.illahi@medicaboo.com>
2021-01-24 13:47:10 +0700

    Update 11-binary-tree-class-func.js

--- 1-basic/data-structure/11-binary-tree-class-func.js
+++ 1-basic/data-structure/11-binary-tree-class-func.js
@@ -4,3 +4,44 @@ function Node(value) {
     this.left = null;
     this.right = null;
 }
+
+function BinarySearchTree() {
+
+    this.root = null;
+
+    this.add = function (integer) {
+
+        let curr = this.root;
+
+        if(!curr) {
+            this.root = new Node(integer);
+            return undefined;
+        };
+
+        while(curr) {
+            if (integer < curr.value) {
+                if(!curr.left) {
+                    curr.left = new Node(integer);
+                    return undefined;
+                }
+                curr = curr.left;
+            }
+            else if (integer > curr.value) {
+                if (!curr.left) {
+                    curr.left = new Node(integer);
+                    return undefined;
+                }
+                curr = curr.left;
+    
+            }
+            else if (integer > curr.value) {
+                if (!curr.right) {
+                    curr.right = new Node(integer);
+                    return undefined;
+                }
+                curr = curr.right;
+            }
+            else return null;
+        }
+    }
+}
