341e73c2a6b1d68c84fad24593e52854a550dd59
Fajr <fajri.illahi@medicaboo.com>
2021-01-24 13:36:13 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/data-structure/tempCodeRunnerFile.js
+++ 1-basic/data-structure/tempCodeRunnerFile.js
@@ -1,3 +1,38 @@
-if (hashedObj.hasOwnProperty(key)) {
-      delete hashedObj[key];
+var displayTree = tree => console.log(JSON.stringify(tree, null, 2));
+function Node(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+function BinarySearchTree() {
+
+  this.root = null;
+  // change code below this line
+  this.add = function (integer) {
+
+    let curr = this.root;
+
+    if (!curr) {
+      this.root = new Node(integer);
+      return undefined;
+    };
+    
+    while (curr) {
+      if (integer < curr.value) {
+        if (!curr.left) {
+          curr.left = new Node(integer);
+          return undefined;
+        }
+        curr = curr.left;
+      }
+      else if (integer > curr.value) {
+        if (!curr.right) {
+          curr.right = new Node(integer);
+          return undefined;
+        }
+        curr = curr.right;
+      }
+      else return null;
     }
+  }   
+}
