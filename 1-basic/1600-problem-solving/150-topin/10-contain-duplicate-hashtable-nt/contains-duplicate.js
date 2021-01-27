15b12a05a652db1392eac4c6a8cc9c53e7d4cef3
Fajr <fajri.illahi@medicaboo.com>
2021-01-27 07:19:13 +0700

    Create contains-duplicate.js

---
+++ 1-basic/1600-problem-solving/150-topin/10-contain-duplicate-hashtable-nt/contains-duplicate.js
@@ -0,0 +1,13 @@
+var containsDuplicate = function(nums) {
+    count = {}
+    for (c of nums) {
+        count[c] = (count[c] || 0) + 1;
+    }
+    console.log(count);
+    for (c in count) {
+        if(count[c] > 1) return true;
+    }
+    return false;
+};
+
+console.log(containsDuplicate([1,2,3,1]));
