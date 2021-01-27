c75f5584bdb6cad0952a5b209f5d7ab17bae9745
Fajr <fajri.illahi@medicaboo.com>
2021-01-27 07:19:51 +0700

    Update delete-node-linkedlist.js

--- 1-basic/1600-problem-solving/150-topin/4-delete-node-linkedlist-nt/delete-node-linkedlist.js
+++ 1-basic/1600-problem-solving/150-topin/4-delete-node-linkedlist-nt/delete-node-linkedlist.js
@@ -10,13 +10,63 @@
  * @return {void} Do not return anything, modify node in-place instead.
  */
 
-function ListNode(val) {
-    this.val = val;
+function Node (val) {
+    this.element = val;
     this.next = null;
 }
 
-var deleteNode = function(node) {
-    [node.val, node.next.val] = [node.next.val, node.val];
-    node.next = node.next.next;
+function ListNode() {
+    head = null;
+    length = 0;
 
+    this.head = () => {
+        return head
+    }
+
+    this.add = function (val) {
+        node = new Node(val);
+        
+        if (head === null) {
+            head = node;
+            
+        } else {
+            
+            current = head;
+            while(current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+
+        length++;
+    }
+
+    this.deleteNode = function(node) {
+        node.element = node.next.element;
+        node.next = node.next.next;
+        return head = node;
+    
+    }
+    
+    this.print = () => {
+        current = head;
+        while(length) {
+            console.log(current.element);
+            current = current.next;
+            length--;
+        }
+        
+        
+    }
 }
+
+
+var ln = new ListNode();
+ln.add(200);
+ln.add(300);
+ln.add(400);
+ln.add(500);
+// ln.deleteNode( { element: 200, next:  { element: 300, next: null } });
+console.log(ln);
+console.log(ln.head())
+ln.print();
