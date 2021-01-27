c36cd4a0593ad2f96e0d43f4533ae0a039364cb9
Fajr <fajri.illahi@medicaboo.com>
2021-01-27 07:19:22 +0700

    Create Solution.java

---
+++ 1-basic/1600-problem-solving/150-topin/9-valid-anagram/Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution {
+
+    static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int [] counter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+        for (int count : counter) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagrama";
+        System.out.println(isAnagram(s,t));
+    }
+}
