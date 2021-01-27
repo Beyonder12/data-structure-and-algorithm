0097e4634c49ea2d90e12ff960781d6a37afd732
Fajr <fajri.illahi@medicaboo.com>
2021-01-27 07:19:35 +0700

    Update input.js

--- 1-basic/input.js
+++ 1-basic/input.js
@@ -1,22 +1,37 @@
-var twoSum = function(nums, target) {
-  const arr = [];//array for storing parse values
-  let i = 0; // counter
-  while( i < nums.length ){
-      //checking in current number exist in array or not
-      if( arr[ target - nums[i] ] != undefined )  return [  arr[ target - nums[i] ], i ];
-      arr[ nums[i] ] = i;//adding parse value to array
-      console.log(arr)
-      i++;//increamenting the counter
-      
+const { performance } = require('perf_hooks'); //Timing library
+
+function objectSolution(nums) {
+  let testObj = {};
+  for (var i = 0; i < nums.length; i++) {
+    let aNum = nums[i];
+    if (testObj[aNum]) {
+      return true;
+    } else {
+      testObj[aNum] = true;
+    }
   }
-};
 
-let nums = [2,11,15,7], target = 9;
+  return false;
+}
 
-console.log(twoSum(nums, target));
+function setSolution(nums) {
+  let testSet = new Set(nums);
+  return testSet.size !== nums.length;
+}
 
-a = [];
+function runTest(testFunction, testArray) {
+  console.log('   Running test:', testFunction.name);
+  let start = performance.now();
+  let result = testFunction(testArray);
+  let end = performance.now();
+  console.log('      Duration:', end - start);
+}
 
-a[-2] = 0;
-a[1] = 0;
-console.log(a)
+let arr = [];
+let setSize = 1000;
+for (var i = 0; i < setSize; i++) {
+  arr.push(i);
+}
+console.log('Set size:', setSize);
+runTest(objectSolution, arr);
+runTest(setSolution, arr);
