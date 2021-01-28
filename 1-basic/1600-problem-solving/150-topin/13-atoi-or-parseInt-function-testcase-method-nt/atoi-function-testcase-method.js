3baadc8fe9d706e9a522c62d2791652428111ece
Fajr <fajri.illahi@medicaboo.com>
2021-01-28 09:42:54 +0700

    Create atoi-function-testcase-method.js

---
+++ 1-basic/1600-problem-solving/150-topin/13-atoi-or-parseInt-function-testcase-method-nt/atoi-function-testcase-method.js
@@ -0,0 +1,25 @@
+var myAtoi = function (s) {
+    s = s.trim();
+    if(!s) return 0;
+
+    var sign = 1;
+    var i = 0, answer = 0;
+    if(s[i] == '+') {
+        sign = 1;
+        i++;
+    }
+
+    else if(s[i] == '-') {sign=-1;i++}
+
+    for(;i<s.length;i++) {
+        var temp=s.charCodeAt(i)-48;
+        console.log(answer);
+        if(temp>9 || temp < 0)break;
+        if(answer > 2147483647/10 || answer > (2147483647-temp)/10)return sign == 1 ? 2147483647 : -2147483648;
+        else answer = answer * 10 + temp;
+    }
+    return answer* sign;
+}
+
+console.log(myAtoi("123"))
+console.log("1".charCodeAt())
