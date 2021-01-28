7fe7b3ba838c51aef3b1251d21e6ac9d65ac44ff
Fajr <fajri.illahi@medicaboo.com>
2021-01-28 09:42:58 +0700

    Create Solution.java

---
+++ 1-basic/1600-problem-solving/150-topin/13-atoi-or-parseInt-function-testcase-method-nt/Solution.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+
+public class Solution {
+
+    public static int myAtoi(String str) {
+        str = str.trim();
+        if (str.isEmpty())
+            return 0;
+        int sign = 1; int i = 0;
+        if (str.charAt(0) == '-' || str.charAt(0) == '+') {
+            sign = (str.charAt(0) == '-') ? -1 : 1;
+            if (str.length() < 2 || !Character.isDigit(str.charAt(1))) {
+                return 0;
+            }
+            i++;
+        }
+        int n = 0;
+        while (i < str.length()) {
+            if (Character.isDigit(str.charAt(i))) {
+                int d = str.charAt(i) - '0';
+                if (n > (Integer.MAX_VALUE - d) / 10) { //Detect the integer
+                    n = (sign == -1) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+                    return n;
+                }
+                n = n*10 + d;
+            } else {
+                break;
+            }
+            i++;
+        }
+        return sign * n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myAtoi("a2ii430jcm"));
+    }
+    
+}
