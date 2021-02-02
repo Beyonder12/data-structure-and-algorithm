d1d042ab228cb4583122800767b64019c75907d0
Fajr <fajri.illahi@medicaboo.com>
2021-02-02 09:00:03 +0700

    Create Solution.java

---
+++ 1-basic/1600-problem-solving/150-topin/15-romantoInteger-hashMapMethod-nt/Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution {
+
+    public static int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int res = 0;
+        for(int i = 0; i < s.length() - 1; i++) {
+            if(map.get(s.charAt(i)) < map.get(s.charAt(i+1))) res -= map.get(s.charAt(i));
+            else res += map.get(s.charAt(i));
+        }
+        return res + map.get(s.charAt(s.length() - 1));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(romanToInt("IV"));
+    }
+    
+}
