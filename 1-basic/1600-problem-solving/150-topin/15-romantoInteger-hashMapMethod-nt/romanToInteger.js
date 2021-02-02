4b7f3e7a9ad77d86922e92cf41bbb681753feeba
Fajr <fajri.illahi@medicaboo.com>
2021-02-02 09:00:07 +0700

    Create romanToInteger.js

---
+++ 1-basic/1600-problem-solving/150-topin/15-romantoInteger-hashMapMethod-nt/romanToInteger.js
@@ -0,0 +1,19 @@
+symbols = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+};
+
+var romanToInt = function(s) {
+    value = 0;
+    for(let i = 0; i < s.length; i++) {
+        symbols[s[i]] < symbols[s[i+1]] ? value -= symbols[s[i]] : value+= symbols[s[i]];
+    }
+    return value;
+}  
+
+console.log(1>undefined)
