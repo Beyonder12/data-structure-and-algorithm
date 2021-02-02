dc47dec38ce9526330632b40a4bee70496c0787d
Fajr <fajri.illahi@medicaboo.com>
2021-02-02 08:59:44 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/data-structure/tempCodeRunnerFile.js
+++ 1-basic/data-structure/tempCodeRunnerFile.js
@@ -1,15 +1,19 @@
-function inverseString(s) {
-    let i = 0, j = s.length-1;
-
-    while(i < j) {
-
-        return (s[i]!=s[j]) ?  false :  true ;
-        i++;j--;
+var sumOddLengthSubarrays = function(arr) {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i+=2) {
+        for(let j = 0; j < arr.length; j++) {
+            arr1=[...arr]
+            console.log(arr1)
+            arr1.splice(j,i+1)
+            // sum+=(arr.slice(j,i+1)).reduce((a,c)=>a+c);
+        }
+        // console.log(i+1)
     }
-    
-    
+    return sum;
 }
 
-console.log('fmie')
-console.log(inverseString('bakabi'));
+// sumOddLengthSubarrays([1,2,3,4,5])
 
+a = [1,2,3,4,5]
+console.log(a.slice(3,5))
+console.log(a)
