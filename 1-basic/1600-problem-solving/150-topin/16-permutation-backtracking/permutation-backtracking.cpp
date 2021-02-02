452c00b5daeb21b4d4e80231ec0df7a85e24326a
Fajr <fajri.illahi@medicaboo.com>
2021-02-02 09:00:00 +0700

    Create permutation-backtracking.cpp

---
+++ 1-basic/1600-problem-solving/150-topin/16-permutation-backtracking/permutation-backtracking.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+class Solution {
+    public:
+        
+    static void permute(vector<int> nums, int i, vector< vector<int> > &ans) {
+        if(i == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+        for(int j = i; j < nums.size(); j++) {
+            swap(nums[i],nums[j]);
+            permute(nums,i+1,ans);
+            swap(nums[i],nums[j]);
+        }
+    }
+    static vector< vector<int> > permute(vector<int>& nums) {
+        vector< vector<int> > ans;
+        permute(nums,0,ans);
+        return ans;
+    }
+};
+int main() {
+    int arr[] = {1,2,3};
+    cout << Solution::permute(arr) << endl;
+}
+
+// int main() {
+//     int arr[] = {1,2,3};
+//     for (int i = 0; i < *(&arr + 1) - arr; i++) {
+//         cout << << endl;
+//     }
+    
+// }
