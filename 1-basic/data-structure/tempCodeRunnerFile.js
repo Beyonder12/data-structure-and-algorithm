bc9d6d7dc7d79b3dddbf48c27aa70836f0e00c2a
Fajr <fajri.illahi@medicaboo.com>
2021-02-03 16:21:05 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/data-structure/tempCodeRunnerFile.js
+++ 1-basic/data-structure/tempCodeRunnerFile.js
@@ -1,19 +1,13 @@
-var sumOddLengthSubarrays = function(arr) {
-    let sum = 0;
-    for(let i = 0; i < arr.length; i+=2) {
-        for(let j = 0; j < arr.length; j++) {
-            arr1=[...arr]
-            console.log(arr1)
-            arr1.splice(j,i+1)
-            // sum+=(arr.slice(j,i+1)).reduce((a,c)=>a+c);
+var countPrimes = function(n) {
+    let hash = new Array(n).fill(true);
+    hash[0] = false;
+    hash[1] = false;
+    for (let i=2;i*i<n;i++) {
+        if (hash[i]) {
+            for(let j=i*i;j<n;j+=i){ // p*(p+1)...
+                hash[j] = false;
+            }
         }
-        // console.log(i+1)
     }
-    return sum;
-}
-
-// sumOddLengthSubarrays([1,2,3,4,5])
-
-a = [1,2,3,4,5]
-console.log(a.slice(3,5))
-console.log(a)
+    return hash.filter((val)=>val).length;
+};
