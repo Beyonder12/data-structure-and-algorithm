dfb68636ca8e4d99e03c6392ef5bc0f601fa0a34
Fajr <fajri.illahi@medicaboo.com>
2021-02-03 16:21:14 +0700

    Create prime-number-count-sieve-of-erothesnes.js

---
+++ 1-basic/1600-problem-solving/150-topin/17-count-primes-sieve-erasthotenes-nt/prime-number-count-sieve-of-erothesnes.js
@@ -0,0 +1,13 @@
+var countPrimes = function(n) {
+    let hash = new Array(n).fill(true);
+    hash[0] = false;
+    hash[1] = false;
+    for (let i = 2; i*i < n; i++) {
+        if(hash[i]) {
+            for(let j=i*i; j<n; j+=i) { // p*(p+1)...
+                hash[j] = false;
+            }
+        }
+    }
+    return hash.filter(val=>val).length;
+}
