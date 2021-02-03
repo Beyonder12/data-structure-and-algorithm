df1a6b39c79b11d94ff14bd3840566f4be86a9ca
Fajr <fajri.illahi@medicaboo.com>
2021-02-04 06:16:42 +0700

    Update input.js

--- 1-basic/input.js
+++ 1-basic/input.js
@@ -1,37 +1,16 @@
-const { performance } = require('perf_hooks'); //Timing library
+function digitOne(nums) {
 
-function objectSolution(nums) {
-  let testObj = {};
-  for (var i = 0; i < nums.length; i++) {
-    let aNum = nums[i];
-    if (testObj[aNum]) {
-      return true;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    nums[i]++;
+    if (nums[i] > 9) {
+      nums[i] = 0;
     } else {
-      testObj[aNum] = true;
+      return nums;
     }
+
   }
-
-  return false;
+  nums.unshift(1);
+  return nums;
 }
 
-function setSolution(nums) {
-  let testSet = new Set(nums);
-  return testSet.size !== nums.length;
-}
-
-function runTest(testFunction, testArray) {
-  console.log('   Running test:', testFunction.name);
-  let start = performance.now();
-  let result = testFunction(testArray);
-  let end = performance.now();
-  console.log('      Duration:', end - start);
-}
-
-let arr = [];
-let setSize = 1000;
-for (var i = 0; i < setSize; i++) {
-  arr.push(i);
-}
-console.log('Set size:', setSize);
-runTest(objectSolution, arr);
-runTest(setSolution, arr);
+console.log(digitOne([9, 9, 9]))
