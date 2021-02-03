f084121728a3bbd6b062fd197c96f2c433fa6862
Fajr <fajri.illahi@medicaboo.com>
2021-02-04 06:16:45 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/data-structure/tempCodeRunnerFile.js
+++ 1-basic/data-structure/tempCodeRunnerFile.js
@@ -1,13 +1,15 @@
-var countPrimes = function(n) {
-    let hash = new Array(n).fill(true);
-    hash[0] = false;
-    hash[1] = false;
-    for (let i=2;i*i<n;i++) {
-        if (hash[i]) {
-            for(let j=i*i;j<n;j+=i){ // p*(p+1)...
-                hash[j] = false;
-            }
-        }
-    }
-    return hash.filter((val)=>val).length;
+rome = function(s) {
+    s.filter(e=>Number(Number.isInteger(e)));
+    
+    return s;
 };
+
+console.log(['123', 'a', 'b', '2'])
+
+console.log(Number.isInteger('1'))
+
+function isLetter(str) {
+     return (str.length === 1 && str.match(/[a-z]/i)) ? true: false;
+  }
+
+  console.log(isLetter('a'))
