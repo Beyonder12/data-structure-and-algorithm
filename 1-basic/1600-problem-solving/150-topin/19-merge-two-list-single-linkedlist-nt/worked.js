e98f50d35aa61e2d69c81c59ba4472645b504d6c
Fajr <fajri.illahi@medicaboo.com>
2021-02-04 20:50:59 +0700

    Create worked.js

---
+++ 1-basic/1600-problem-solving/150-topin/19-merge-two-list-single-linkedlist-nt/worked.js
@@ -0,0 +1,20 @@
+function ListNode(ele) {
+    this.value = ele;
+    this.next = null;
+  }
+  
+  function linkedList(arr){
+    let list = new ListNode(arr[0]);
+    
+    let selectedNode = list;
+    for(let i = 1; i < arr.length; i++){
+      selectedNode.next = new ListNode(arr[i]);
+      selectedNode = selectedNode.next
+    } 
+  
+    return list
+  }
+  
+  l = [3, 1, 2, 3, 4, 5];
+  
+  console.log(linkedList(l));
