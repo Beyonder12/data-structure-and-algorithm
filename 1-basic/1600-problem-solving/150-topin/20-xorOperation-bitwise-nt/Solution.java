65fc14e70d97998dd98c992faae9302bd1fac869
Fajr <fajri.illahi@medicaboo.com>
2021-02-09 00:14:48 +0700

    Update Solution.java

--- 1-basic/1600-problem-solving/150-topin/20-xorOperation-bitwise-nt/Solution.java
+++ 1-basic/1600-problem-solving/150-topin/20-xorOperation-bitwise-nt/Solution.java
@@ -5,13 +5,14 @@ public class Solution {
     public static int xorOperation(int n, int start) {
         int res = start;
         for (int i = 1; i < n; i++) {
-            res = res ^ (start + 2 * i);
+            res ^=  (start + 2 * i);
         }
         return res;
     }
 
     public static void main(String[] args) {
-        System.out.println(xorOperation(5,0));
+        System.out.println(xorOperation(5,1));
+        System.out.println(1^3^5^7^9);
     }
 
 }
