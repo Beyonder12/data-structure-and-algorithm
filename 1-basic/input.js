10fe30448934df2c3e46053d689999043279b5b4
Fajr <fajri.illahi@medicaboo.com>
2021-02-08 16:22:52 +0700

    Update input.js

--- 1-basic/input.js
+++ 1-basic/input.js
@@ -1,14 +1,44 @@
-function sumZero(n) {
-  let result = [], c = Math.ceil(-n / 2), sum = 0;
-  for (let i = 0; i < n; i++) {
+var permArr = [],
+  usedChars = [];
 
-    result.push(c);
-    sum += c;
-    c++;
+function permute(input) {
+  var i, ch;
+  for (i = 0; i < input.length; i++) {
+    ch = input.splice(i, 1)[0];
+    usedChars.push(ch);
+    if (input.length == 0) {
+      permArr.push(usedChars.slice());
+    }
+    permute(input);
+    input.splice(i, 0, ch);
+    usedChars.pop();
   }
-  console.log(result.indexOf(0))
-  if (n%2==0) { result.splice(result.indexOf(0),1,-sum); }
-  return result;
+  return permArr
+};
+
+const permutator = (inputArr) => {
+  let result = [];
+
+  const permute = (arr, m = []) => {
+    if (arr.length === 0) {
+      result.push(m)
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next))
+     }
+   }
+ }
+
+ permute(inputArr)
+
+ return result;
 }
 
-console.log(sumZero(9))
+console.log([[5,1,2,3,4],[1,2,3]].slice())
+console.log(JSON.stringify(permute([5, 3, 7, 1])));
+console.log(permutator([5, 3, 7, 1]));
+
+let a;
+console.log((a.length === true))
