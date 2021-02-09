eea4361df40781be683b8b47d3feb378a7bfe50b
Fajr <fajri.illahi@medicaboo.com>
2021-02-09 13:53:26 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/data-structure/tempCodeRunnerFile.js
+++ 1-basic/data-structure/tempCodeRunnerFile.js
@@ -1,36 +1,23 @@
-var MinStack = function() {
-    this.elements = [];
-  };
-  
-  /**
-  
-   @param {number} x
-   @return {void}
-   */
-  MinStack.prototype.push = function(x) {
-    this.elements.push({
-      value: x,
-      min: this.elements.length === 0 ? x : Math.min(x, this.getMin()),
-    });
-  };
-  /**
-  
-   @return {void}
-   */
-  MinStack.prototype.pop = function() {
-    this.elements.pop();
-  };
-  /**
-  
-   @return {number}
-   */
-  MinStack.prototype.top = function() {
-    return this.elements[this.elements.length - 1].value;
-  };
-  /**
-  
-   @return {number}
-   */
-  MinStack.prototype.getMin = function() {
-    return this.elements[this.elements.length - 1].min;
-  };
+var countAndSay = function(n) {
+  var str = '1';
+  for (var i=1; i < n; i++) {
+      var strArray = str.split('');
+      str ='';
+      var count = 1;
+      // Loop through current nth level line
+      for (var j=0; j < strArray.length; j++) {
+          // Next digit is different
+          if (strArray[j] !== strArray[j+1]) {
+              // Go to next non-matching digit
+              str += count + strArray[j];
+              count = 1;
+          }
+          else {
+              count++;
+          }
+      }
+  }
+  return str;
+};
+
+console.log(countAndSay(5)) 
