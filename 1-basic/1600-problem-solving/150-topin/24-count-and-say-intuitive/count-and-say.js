ff43392ce2ace19d2a982627d297c7fe414d1f4f
Fajr <fajri.illahi@medicaboo.com>
2021-02-09 13:53:37 +0700

    Create count-and-say.js

---
+++ 1-basic/1600-problem-solving/150-topin/24-count-and-say-intuitive/count-and-say.js
@@ -0,0 +1,23 @@
+var countAndSay = function(n) {
+    var str = "1";
+    for (var i = 1; i < n; i++) {
+        var strArray = str.split('');
+        str = '';
+        var count = 1;
+        // loop through current nth level line
+        for (var j = 0; j < strArray.length; j++) {
+            // Next digit is different
+            if (strArray[j] !== strArray[j+1]) {
+                // Go to next non-matching digit
+                str += count + strArray[j];
+                count = 1;
+            }
+            else {
+                count++;
+            }
+        }
+    }
+    return str;
+};
+
+console.log(countAndSay(5))
