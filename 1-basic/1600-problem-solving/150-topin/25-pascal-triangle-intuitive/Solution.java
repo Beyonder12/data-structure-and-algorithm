457a5167945d6f3ba2a8ef98efd4ee67b616363b
Fajr <fajri.illahi@medicaboo.com>
2021-02-10 09:05:42 +0700

    Create Solution.java

---
+++ 1-basic/1600-problem-solving/150-topin/25-pascal-triangle-intuitive/Solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> allrows = new ArrayList<List<Integer>>();
+        ArrayList<Integer> row = new ArrayList<Integer>();
+        for (int i = 0; i < numRows; i++) {
+            row.add(0, 1);
+            for (int j = 1; j < row.size() - 1; j++)
+                row.set(j, row.get(j) + row.get(j + 1));
+            allrows.add(new ArrayList<Integer>(row));
+        }
+        return allrows;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generate(5));
+    }
+}
