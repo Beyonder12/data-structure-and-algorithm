93983a348d5fc01b9e97b7cb71481a0f5b89a0c1
Fajr <fajri.illahi@medicaboo.com>
2021-02-10 09:05:39 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/data-structure/tempCodeRunnerFile.js
+++ 1-basic/data-structure/tempCodeRunnerFile.js
@@ -1,23 +1,24 @@
-var countAndSay = function(n) {
-  var str = '1';
-  for (var i=1; i < n; i++) {
-      var strArray = str.split('');
-      str ='';
-      var count = 1;
-      // Loop through current nth level line
-      for (var j=0; j < strArray.length; j++) {
-          // Next digit is different
-          if (strArray[j] !== strArray[j+1]) {
-              // Go to next non-matching digit
-              str += count + strArray[j];
-              count = 1;
-          }
-          else {
-              count++;
-          }
-      }
-  }
-  return str;
-};
+1
+11
+21
+1211
 
-console.log(countAndSay(5)) 
+
+function countAccumulateIt(n) {
+
+  let str = "1";
+
+  for (let i  = 0; i < n; i ++) {
+    let count = 1, newStr=str;
+    for (let j = 0; j < newStr.length; j++) {
+      if(newStr[j] === newStr[j+1]) count++;
+      else newStr = str[j] + count;
+    }
+    
+  }
+
+  return str;
+}
+
+console.log(countAccumulateIt(4))
+console.log("hello")
