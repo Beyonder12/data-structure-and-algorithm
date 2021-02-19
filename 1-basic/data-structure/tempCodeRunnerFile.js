5523b1085226a4b4562fa784b07cbf9ca66f4eec
Fajr <fajri.illahi@medicaboo.com>
2021-02-19 09:38:02 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/data-structure/tempCodeRunnerFile.js
+++ 1-basic/data-structure/tempCodeRunnerFile.js
@@ -1,20 +1,8 @@
-
-
 function Node (val, next = null) {
   this.element = val;
   this.next = next;
-
 }
 
+let node = new Node(100,200);
 
-// function LinkedList() {
-//   var head = null;
-  
-//   this.add = function(val) {
-//     let node = new Node(val);
-//     while
-//   }
-// }
-
-var node = new Node(100);
 console.log(node)
