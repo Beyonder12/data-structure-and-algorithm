28c722afe9aa90629deb445216dde0641d77179e
Fajr <fajri.illahi@medicaboo.com>
2021-02-22 04:46:15 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/data-structure/tempCodeRunnerFile.js
+++ 1-basic/data-structure/tempCodeRunnerFile.js
@@ -1,8 +1,15 @@
-function Node (val, next = null) {
-  this.element = val;
-  this.next = next;
+
+function power(x=3,y) {
+
+  if(y === 0) return 1;
+
+  pow = Math.abs(y);
+
+  let result =  (pow % 2 === 0) ? power(x*x, pow/2) : x*power(x*x, (pow-1)/2);
+
+  return (y>0) ? result : 1 / result;
+
+ 
 }
 
-let node = new Node(100,200);
-
-console.log(node)
+console.log(power(2))
