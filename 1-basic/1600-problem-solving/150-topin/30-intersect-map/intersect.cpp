e445208c4204890e01a2759a22992ba283bc15ff
Fajr <fajri.illahi@medicaboo.com>
2021-02-22 04:46:01 +0700

    Create intersect.cpp

---
+++ 1-basic/1600-problem-solving/150-topin/30-intersect-map/intersect.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> dict;
+        vector<int> res;
+        for(int i = 0; i < (int)nums1.size(); i++) dict[nums1[i]]++;
+        for(int i = 0; i < (int)nums2.size(); i++)
+            if(--dict[nums2[i]] >= 0) res.push_back(nums2[i]);
+        return res;
+    }
+};
+
+int main() {
+
+    cout  << "hello" << endl;
+
+    return 0;
+}
