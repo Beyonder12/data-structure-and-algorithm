90ca27b02ec6f7bb08a04fa0e4bd92ee7d71720a
Fajr <fajri.illahi@medicaboo.com>
2021-02-22 04:45:51 +0700

    Update remove-duplicate-two-pointers.js

--- 1-basic/1600-problem-solving/150-topin/22-remove-duplicate-enhancedmethod/remove-duplicate-two-pointers.js
+++ 1-basic/1600-problem-solving/150-topin/22-remove-duplicate-enhancedmethod/remove-duplicate-two-pointers.js
@@ -3,8 +3,14 @@ var removeDuplicates = function(nums) {
     for(let j = 0; j < nums.length; j++) {
         if (nums[j] != nums[i])
             nums[++i] = nums[j];
+
     }
+    console.log(nums)
     return ++i;
 }
 
-console.log(removeDuplicates([1,2]))
+console.log(removeDuplicates([1,2,2,2,2,3,4,5,5,5,5]))
+
+// let a = [1,2,3]
+// let b=0;
+// console.log(a[++b]); 
