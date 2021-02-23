36fe738344073537aae1ee6a468d758868c3f2db
Fajr <fajri.illahi@medicaboo.com>
2021-02-23 09:06:27 +0700

    Create isPower3.js

---
+++ 1-basic/1600-problem-solving/150-topin/31-check-is-power-three-maximumdigit/isPower3.js
@@ -0,0 +1,18 @@
+var isPowerOfThree = function(n) {
+    let m = 1, base = 3;
+
+    while (true) {
+        if ( m < n ) {
+            m *= base;
+        } else if ( m > n) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
+
+console.log(isPowerOfThree(1/27))
+
+// this is bery impressive!!!
+//
