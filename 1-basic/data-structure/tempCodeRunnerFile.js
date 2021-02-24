b0e95f523c81add8aee6bd07f5efb3fe36687ee7
Fajr <fajri.illahi@medicaboo.com>
2021-02-24 16:58:34 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/data-structure/tempCodeRunnerFile.js
+++ 1-basic/data-structure/tempCodeRunnerFile.js
@@ -1,14 +1,1 @@
- function runningSum(nums) {
-  let a = [];
-
-  for(let i = 0; i < nums.length; i++) {
-    sum = 0;
-    for(let j = 0; j <=i; j++) {
-      sum+=nums[j]
-    }
-    a.push(sum);
-  }
-
-  return a;
-}
-console.log(runningSum([1,2,3]));
+console.log(1+null)
