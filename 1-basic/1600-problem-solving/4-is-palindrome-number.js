83d2a1a65505942076fe18f79281ee88bce6de27
Fajr <fajri.illahi@medicaboo.com>
2021-02-27 00:09:50 +0700

    Create 4-is-palindrome-number.js

---
+++ 1-basic/1600-problem-solving/4-is-palindrome-number.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if(x<0) return false;
+    let y = x, res = 0;
+    while(y) {
+        remain = y % 10;
+        res = res*10 + remain; 
+        y = Math.floor(y /10);
+    }
+    
+    return x === res;
+    
+  };
+  
+  console.log(isPalindrome(-121))
+  
+  // x = 123;
+  // y = Math.floor(x /10);
+  // console.log(y)
+  // y = Math.floor(x /10);
+  // console.log(y)
+  
+  
