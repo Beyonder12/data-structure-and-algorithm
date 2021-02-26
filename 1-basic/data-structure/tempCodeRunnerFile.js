3cb3c69b968e3d9323986e8d9e8da8e3a3f53b62
Fajr <fajri.illahi@medicaboo.com>
2021-02-26 11:56:37 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/data-structure/tempCodeRunnerFile.js
+++ 1-basic/data-structure/tempCodeRunnerFile.js
@@ -1,1 +1,20 @@
-ode;
+// function a(nums) {
+  
+//   nums.sort((a,b)=>a-b);
+//   let l = nums.length - 1, ans = [], sumMax=0;
+//   for(let i = 0; i < nums.length; i++) {
+    
+//     sumMax += nums[nums.length - 1 -i];
+//     if(sumMax > nums.splice()) return ans.push(nums[nums.length - 1 -i])
+
+//   }
+
+//   return ans;
+// }
+
+
+
+let test = [1,2,3];
+console.log(test.push(1));
+
+console.log(test)
