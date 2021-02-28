fc47a4ecfd307dc9e6657df483bd76c19baddfa7
Fajr <fajri.illahi@medicaboo.com>
2021-02-28 07:55:41 +0700

    Update add-two-sum.js

--- 1-basic/1600-problem-solving/150-topin/37-add-two-sum-linked-list/add-two-sum.js
+++ 1-basic/1600-problem-solving/150-topin/37-add-two-sum-linked-list/add-two-sum.js
@@ -6,29 +6,30 @@
   }
  
 //   console.log(new ListNode(100))
+
+
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
+var addTwoNumbers = function(l1, l2) {
+    var List = new ListNode(0);
+    var head = List;
+    var sum = 0;
+    var carry = 0;
 
- var addTwoNumbers = function(l1, l2) {
-     var List = new ListNode(0);
-     var head = List;
-     var sum = 0;
-     var carry = 0;
+    while(l1!==null||l2!==null||sum>0){
 
-     while(l1!==null||l2!==null||sum>0) {
-
-        if(l1!==null) {
+        if(l1!==null){
             sum = sum + l1.val;
             l1 = l1.next;
         }
-        if(l2!==null) {
+        if(l2!==null){
             sum = sum + l2.val;
             l2 = l2.next;
         }
-        if(sum>=10) {
+        if(sum>=10){
             carry = 1;
             sum = sum - 10;
         }
@@ -39,10 +40,10 @@
         sum = carry;
         carry = 0;
 
-     }
+    }
 
-     return List.next;
- }
+    return List.next;
+};
 
 
 //  l1 = {value = 1, next = {value: 2, next: { value: 0, next = null}}};
@@ -50,14 +51,18 @@
 
  
 
-l1 = new ListNode(1);
-l2 = new ListNode(1);
+l1 = new ListNode(3);
+l2 = new ListNode(4);
 
-l1.next = new ListNode(2);
-l2.next = new ListNode(2);
+l1.next = new ListNode(4);
+l2.next = new ListNode(6);
 
-l1 = JSON.parse(JSON.stringify(l1));
-l2 = JSON.parse(JSON.stringify(l2));
+l1.next.next = new ListNode(2);
+l2.next.next = new ListNode(5);
+
+// l1 = JSON.parse(JSON.stringify(l1));
+// l2 = JSON.
+// parse(JSON.stringify(l2));
 
 console.log(l1);
 console.log(l2);
