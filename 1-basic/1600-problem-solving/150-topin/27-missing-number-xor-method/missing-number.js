129938a66ead105cd43db2a0cf4e365acd52708b
Fajr <fajri.illahi@medicaboo.com>
2021-02-28 07:55:26 +0700

    Update missing-number.js

--- 1-basic/1600-problem-solving/150-topin/27-missing-number-xor-method/missing-number.js
+++ 1-basic/1600-problem-solving/150-topin/27-missing-number-xor-method/missing-number.js
@@ -39,14 +39,14 @@ function missingNumber(nums) {
     for (let i = 0; i < nums.length; i++) {
         res ^= i;
         res ^= nums[i];
-        console.log(res)
+       
         // res = res ^ i ^ nums[i];
     }   
-    return ;
+    return res;
 }
 
 
-let nums = [45,35,38,13,12,23,48,15,44,21,43,26,6,37,1,19,22,3,11,32,4,16,28,49,29,36,33,8,9,39,46,17,41,7,2,5,27,20,40,34,30,25,47,0,31,42,24,10,14]
+let nums = [45,35,38,13,12,23,48,15,44,21,43,26,6,37,19,22,18,11,32,4,16,28,49,29,36,33,8,9,39,46,17,41,7,2,5,27,20,40,34,30,25,47,0,31,42,24,10,14]
 console.log(JSON.stringify(missingNumber(nums)))
 
 // let nums = [1,3,5];
