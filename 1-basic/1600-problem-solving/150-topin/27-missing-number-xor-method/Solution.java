34f3a1326693eb97cca2782302cf1ac3ef7722a2
Fajr <fajri.illahi@medicaboo.com>
2021-02-28 07:55:34 +0700

    Update Solution.java

--- 1-basic/1600-problem-solving/150-topin/27-missing-number-xor-method/Solution.java
+++ 1-basic/1600-problem-solving/150-topin/27-missing-number-xor-method/Solution.java
@@ -2,13 +2,17 @@ import java.util.*;
 
 class Solution {
 
-    public static int hitung(int n) {
-        for (int i = 0; i <=n; i++) {
-            return (i);
+    public static int missingNumber(int[] nums) {
+        int res = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= i;
+            res ^= nums[i];
         }
-    }
-    public static void main(String[] args) {
-        System.out.println(hitung(10));
+        return res;
     }
 
+    public static void main(String[] args) {
+        int [] nums = new int[] {45,35,38,13,12,23,48,15,44,21,43,26,6,37,1,19,22,3,11,32,4,16,28,49,29,36,33,8,9,39,46,17,41,7,2,5,27,20,40,34,30,25,47,0,31,42,24,10,14};
+        System.out.println(missingNumber(nums));
+    }
 }
