ef56119a4ee7be6be26f9fc2b88870490326618f
Fajr <fajri.illahi@medicaboo.com>
2021-03-01 10:01:02 +0700

    Update convert-sort-arrat-to-bst.js

--- 1-basic/1600-problem-solving/150-topin/38-convert-sort-array-to-bst-recursive/convert-sort-arrat-to-bst.js
+++ 1-basic/1600-problem-solving/150-topin/38-convert-sort-array-to-bst-recursive/convert-sort-arrat-to-bst.js
@@ -10,6 +10,10 @@ function TreeNode(val, left, right) {
 // Output: [0,-3,9,-10,null,5]
 // Explanation: [0,-10,5,null,-3,null,9] is also accepted:
 
+// Input: nums = [1,3]
+// Output: [3,1]
+// Explanation: [1,3] and [3,1] are both a height-balanced BSTs.
+
 /**
  * @param {number[]} nums
  * @return {TreeNode}
