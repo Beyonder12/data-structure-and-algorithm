0745c51235a3898798d98ed18705bec795e83957
Fajr <fajri.illahi@medicaboo.com>
2021-03-01 17:25:05 +0700

    Update input2.js

--- 1-basic/input2.js
+++ 1-basic/input2.js
@@ -1,33 +1,7 @@
-const squares = {'0':0, '1':1, '2':4, '3':9, '4':16, '5':25, '6':36, '7':49, '8':64, '9':81}
+function factorial(x) {
+    if(x === 0) return 1;
 
-/**
- * @param {number} n
- * @return {boolean}
- */
-var code = function(n) {
-    /**
-	 * Not necessary I think, but the OJ algorithm appears
-	 * to return false for non-positive numbers also.
-	 */
-    
-	
-	/**
-	 * This set will store numbers as we create them.
-	 * If we create a number already in the set, we
-	 * assume we're in an infinite cycle.
-	 */
-    let set = new Set();
-	
-    while (!set.has(n)) {
-        set.add(n);
-        let s = n.toString();
-        n = 0;
-        for (let i = 0; i < s.length; ++i) {
-            n += squares[s[i]];
-        }
-        if (n == 1) return true;
-    }
-	
-    return false;
-};
-console.log(code(13))
+    return factorial(x-1)*x;
+}
+
+console.log(factorial(5))
