676a52745faa8ae034f9d77e8f9fe6f0ec09516b
Fajr <fajri.illahi@medicaboo.com>
2021-03-01 10:00:55 +0700

    Update input2.js

--- 1-basic/input2.js
+++ 1-basic/input2.js
@@ -1,17 +1,33 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const squares = {'0':0, '1':1, '2':4, '3':9, '4':16, '5':25, '6':36, '7':49, '8':64, '9':81}
 
-rl.question("What is your name ? ", function(name) {
-    rl.question("Where do you live ? ", function(country) {
-        console.log(`${name}, is a citizen of ${country}`);
-        rl.close(); 
-    });
-});
-
-rl.on("close", function() {
-    console.log("\nBYE BYE !!!");
-    process.exit(0);
-});
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var code = function(n) {
+    /**
+	 * Not necessary I think, but the OJ algorithm appears
+	 * to return false for non-positive numbers also.
+	 */
+    
+	
+	/**
+	 * This set will store numbers as we create them.
+	 * If we create a number already in the set, we
+	 * assume we're in an infinite cycle.
+	 */
+    let set = new Set();
+	
+    while (!set.has(n)) {
+        set.add(n);
+        let s = n.toString();
+        n = 0;
+        for (let i = 0; i < s.length; ++i) {
+            n += squares[s[i]];
+        }
+        if (n == 1) return true;
+    }
+	
+    return false;
+};
+console.log(code(13))
