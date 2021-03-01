9dde38849a93febeb6ff16e58a51db5d80a9e742
Fajr <fajri.illahi@medicaboo.com>
2021-03-01 17:25:03 +0700

    Create happy.js

---
+++ 1-basic/1600-problem-solving/150-topin/40-happy/happy.js
@@ -0,0 +1,41 @@
+const squares = {
+    '0':0,
+    '1':1,
+    '2':4,
+    '3':9,
+    '4':16,
+    '5':25,
+    '6':36,
+    '7':49,
+    '8':64,
+    '9':81,
+}
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+ var happyNumber = function(n) {
+     // OJ algorithm appears to return false for non-positive numbers also
+     // This set will store numbers as we create them. If we create a number
+     // already in the set, we assume we're in an infinite cycle.
+
+     let set = new Set();
+
+     while(!set.has(n)) {
+         set.add(n);
+         let s = n.toString();
+         n = 0;
+         for(let i = 0; i < s.length; i++) {
+             n += squares[s[i]];
+         }
+         if (n == 1) return true;
+     }
+     return false
+ }
+
+ console.log(happyNumber(19));
+ console.log(happyNumber(13));
+ console.log(happyNumber(2));
+ console.log(happyNumber(11));
