630a72183dce1932d444e5f2b38eacca02381987
Fajr <fajri.illahi@medicaboo.com>
2021-03-03 06:43:32 +0700

    Create 10-sum-each-element.js

---
+++ 1-basic/1600-problem-solving/10-sum-each-element.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+ var runningSum = function(nums) {
+     let a = [];
+     for(let i = 0; i < nums.length; i++) {
+         sum = 0;
+         for(let j = 0; j <= i; j++) {
+             sum += nums[j];
+         };
+         a.push(sum);
+     };
+     return a;
+ };
+
+ console.log(runningSum([1,2,3,4,5]));
+
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
