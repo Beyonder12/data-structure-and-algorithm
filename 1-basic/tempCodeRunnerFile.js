ae3c92eda0681f36e76450aa9889306c63a0df8a
Fajr <fajri.illahi@medicaboo.com>
2021-03-04 17:57:34 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/tempCodeRunnerFile.js
+++ 1-basic/tempCodeRunnerFile.js
@@ -1,1 +1,23 @@
-(".", "[.]");
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+    let i = 0, j = 0, count = 0;
+    while(jewels.length - i) {
+        // console.log(i)
+        i++;
+        while(stones.length-j) {
+            console.log(j)
+           if (jewels[i] === stones[j]) count++;
+           j++;
+        }
+    }
+    
+    // return count;
+};
+
+jewels = "aA", stones = "aAAbbbb"
+
+console.log(numJewelsInStones(jewels, stones))
