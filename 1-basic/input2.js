acd949961548e85ca07cf127a76739691cf55fc5
Fajr <fajri.illahi@medicaboo.com>
2021-04-13 15:04:46 +0700

    Update input2.js

--- 1-basic/input2.js
+++ 1-basic/input2.js
@@ -1,18 +1,25 @@
-/**
- * @param {number} n
- * @return {number}
- */
-var subtractProductAndSum = function(n) {
-    let product = 1, sum = 0, remain;
-    while(n) {
-        remain = n%10;
-        product *= remain;
-        sum += remain
-        n = Math.floor(n/10);
-    }
-    
-    return product - sum;
-    
-};
 
-console.log(subtractProductAndSum(234))
+
+myArray = [
+  {receiveId: "one", stockAfter: "red"},
+  {receiveId: "two", stockAfter: "blue"},
+  {receiveId: "one", stockAfter: "green"},
+  {receiveId: "one", stockAfter: "black"}
+]
+
+
+
+var groups = {};
+for (var i = 0; i < myArray.length; i++) {
+  var groupName = myArray[i].receiveId;
+  if (!groups[groupName]) {
+    groups[groupName] = [];
+  }
+  groups[groupName].push(myArray[i].stockAfter);
+}
+myArray = [];
+for (var groupName in groups) {
+  myArray.push({receiveId: groupName, stockAfter: groups[groupName]});
+}
+
+console.log(myArray);
