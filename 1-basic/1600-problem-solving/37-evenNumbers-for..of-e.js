2622029c47bc86855d34863c59275868e971f0a5
Fajr <fajri.illahi@medicaboo.com>
2021-04-13 15:04:48 +0700

    Create 37-evenNumbers-for..of-e.js

---
+++ 1-basic/1600-problem-solving/37-evenNumbers-for..of-e.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @param {number[]} nums 
+ * @returns {number}
+ */
+
+var findNumbers = function(nums) {
+    let count = 0;
+    for(val of nums) {
+      
+        if(JSON.stringify(val).length % 2 === 0) count++;
+    }
+    
+    return count;
+  };
+  
+  console.log(findNumbers([12,345,2,6,7896]))
