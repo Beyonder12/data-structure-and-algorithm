5078c3f48c6de70c83ec07b864fb7fe5087e9c4a
Fajr <fajri.illahi@medicaboo.com>
2021-04-14 11:38:19 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/tempCodeRunnerFile.js
+++ 1-basic/tempCodeRunnerFile.js
@@ -1,11 +1,18 @@
-var findNumbers = function(nums) {
-  let count = 0;
-  for(val of nums) {
-    
-      if(JSON.stringify(val).length % 2 === 0) count++;
-  }
+var halvesAreAlike = function(s) {
+  let alike = 0, vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
   
-  return count;
+  for(let i = 0 ; i < s.length; i++) {
+    if(i < s.length/2 && vowels.includes(s[i])) {
+      alike++ ;
+    } else if (i >= s.length/2 && vowels.includes(s[i])) {
+      alike--;
+    }
+    
+    // console.log(vowels.includes(s[i]))
+    // console.log(alike)
+  }
+ 
+  return alike === 0 ? true : false;
 };
 
-console.log(findNumbers([12,345,2,6,7896]))
+console.log(halvesAreAlike('book'))
