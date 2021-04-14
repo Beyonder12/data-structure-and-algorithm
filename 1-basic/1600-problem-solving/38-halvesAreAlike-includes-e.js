84824a52b0c484a30fb8323629c3144608658562
Fajr <fajri.illahi@medicaboo.com>
2021-04-14 11:38:15 +0700

    Create 38-halvesAreAlike-includes-e.js

---
+++ 1-basic/1600-problem-solving/38-halvesAreAlike-includes-e.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @param {string} s 
+ * @returns {boolean}
+ */
+var halvesAreAlike = function(s) {
+    let alike = 0, vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    
+    for(let i = 0 ; i < s.length; i++) {
+      if(i < s.length/2 && vowels.includes(s[i])) {
+        alike++ ;
+      } else if (i >= s.length/2 && vowels.includes(s[i])) {
+        alike--;
+      }
+      
+      // console.log(vowels.includes(s[i]))
+      // console.log(alike)
+    }
+   
+    return alike === 0 ? true : false;
+  };
+  
+  console.log(halvesAreAlike('book'))
