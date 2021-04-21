721bef713b3492d432f8478979e30d262ff3a738
Fajr <fajri.illahi@medicaboo.com>
2021-04-22 02:49:48 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/tempCodeRunnerFile.js
+++ 1-basic/tempCodeRunnerFile.js
@@ -1,53 +1,29 @@
-var freqAlphabets = function(s) {
-  let a = [];
-  const dictionary = {
-      '1' : 'a',
-      '2' : 'b',
-      '3' : 'c',
-      '4' : 'd',
-      '5' : 'e',
-      '6' : 'f',
-      '7' : 'g',
-      '8' : 'h',
-      '9' : 'i',
-      '10#' : 'j',
-      '11#' : 'k',
-      '12#' : 'l',
-      '13#' : 'm',
-      '14#' : 'n',
-      '15#' : 'o',
-      '16#' : 'p',
-      '17#' : 'q',
-      '18#' : 'r',
-      '19#' : 's',
-      '20#' : 't',
-      '21#' : 'u',
-      '22#' : 'v',
-      '23#' : 'w',
-      '24#' : 'x',
-      '25#' : 'y',
-      '26#' : 'z',
-
-  }
-  console.log(s)
-  for(let i = 0; i < s.length; i++) {
-    console.log(s[i])
-      if(s[i+2] === '#') {
-        a .push(dictionary[s[i]+s[i+1]+s[i+2]]);
-        i+=2;
-      }
+var diagonalSum = function(mat) {
+  let sum = 0;
+  for(let i = 0; i < mat.length; i++) {
+      console.log(mat.length - 1 - i)
+      // console.log(mat[i][i])
+      // console.log(mat[i][mat.length - 1 - i])
+      if (i === mat.length - 1 - i) {sum += mat[i][i]; continue;}
       else {
-        a.push(dictionary[s[i]])
+        sum += mat[i][i];
+        sum += mat[i][mat.length - 1 - i]
       }
-      
   }
-  return a.join('');
+  
+  return sum;
   
 };
 
-s = "10#11#12"
-console.log(freqAlphabets(s))
 
+// mat = [[1,2,3,4],
+//       [5,6,7,8],
+//       [9,10,11,12],
+//       [13,14,15,16]]
 
-const map = {"1":"a","2":"b","3":"c","4":"d","5":"e","6":"f","7":"g","8":"h","9":"i","10":"j","11":"k","12":"l","13":"m","14":"n","15":"o","16":"p","17":"q","18":"r","19":"s","20":"t","21":"u","22":"v","23":"w","24":"x","25":"y","26":"z"};
-const freqAlphabets = s => s.replace(/(\d\d#|\d)/g, t => map[t.length === 3 ? t[0] + t[1] : t]);
+mat = [[1,1,1,1],
+      [1,1,1,1],
+      [1,1,1,1],
+      [1,1,1,1]]
+
+console.log(diagonalSum(mat))
