06a3184652e7d4a98b7d2ebfdbf68face26065bb
Fajr <fajri.illahi@medicaboo.com>
2021-04-23 02:41:32 +0700

    Update 50-destinationCity-mapandset-e.js

--- 1-basic/1600-problem-solving/50-destinationCity-mapandset-e.js
+++ 1-basic/1600-problem-solving/50-destinationCity-mapandset-e.js
@@ -5,14 +5,45 @@
  * @param {string[][]} paths 
  * @returns {string}
  */
+
+// SOLUTION 1 - Map
+// We could use a map to store the status of city:
+
+// If it's a start of path, make the value plus 1.
+// If it's a end of path, make the value minus 1.
+// Finally, most cities will be 0. The start of whole path will be 1. And the destination city will be -1.
+
 const destCity = (paths) => {
     const map = new Map();
-
+    console.log(map)
     for (const path of paths) {
         map.set(path[0], map.has(path[0]) ? 0 : 1);
         map.set(path[1], map.has(path[1]) ? 0 : -1);
+        console.log(map)
     }
     for (const item of map) {
         if (item[1] === -1) return item[0];
     }
+};
+
+paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+console.log(destCity(paths))
+
+// SOLUTION 2 - Set
+// Actually, we don't need all informations. So, we may not to store them but just a mark.
+
+// Here we use a Set to store the mark:
+
+// We mark all start of paths.
+// The destination city must not be in this set.
+
+const destCity1 = (paths) => {
+    const set = new Set();
+    console.log(set)
+    for (const path of paths) {set.add(path[0]); console.log(set);};
+    for (const path of paths) {
+        console.log(path)
+        if (!set.has(path[1])) return path[1];
+    }
 }
+console.log(destCity1(paths))
