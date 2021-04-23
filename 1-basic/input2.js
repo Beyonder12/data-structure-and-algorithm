ff65384d203343e73efe13395a2e89474032fd50
Fajr <fajri.illahi@medicaboo.com>
2021-04-24 02:28:23 +0700

    Update input2.js

--- 1-basic/input2.js
+++ 1-basic/input2.js
@@ -1,25 +1,23 @@
 
 
 myArray = [
-  {receiveId: "one", stockAfter: "red"},
-  {receiveId: "two", stockAfter: "blue"},
-  {receiveId: "one", stockAfter: "green"},
-  {receiveId: "one", stockAfter: "black"}
+  {
+    receiveId: "one", 
+    stockAfter: [
+      {name: "halo"},
+      {name: "jeki"}
+    ]
+  },
+  {
+    receiveId: "two", 
+    stockAfter: [
+      {name: "mama"},
+      {name: "papa"},
+    ]
+  },
+  
 ]
 
+console.log(myArray)
 
-
-var groups = {};
-for (var i = 0; i < myArray.length; i++) {
-  var groupName = myArray[i].receiveId;
-  if (!groups[groupName]) {
-    groups[groupName] = [];
-  }
-  groups[groupName].push(myArray[i].stockAfter);
-}
-myArray = [];
-for (var groupName in groups) {
-  myArray.push({receiveId: groupName, stockAfter: groups[groupName]});
-}
-
-console.log(myArray);
+console.log(JSON.stringify(myArray))
