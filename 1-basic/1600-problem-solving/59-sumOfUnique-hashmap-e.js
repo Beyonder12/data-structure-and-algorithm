6772aa02b188f97cd0d04d7ee090c6c3ba227d39
Fajr <fajri.illahi@medicaboo.com>
2021-04-25 18:28:09 +0700

    Create 59-sumOfUnique-hashmap-e.js

---
+++ 1-basic/1600-problem-solving/59-sumOfUnique-hashmap-e.js
@@ -0,0 +1,40 @@
+/**
+ * 
+ * @param {number[]} nums 
+ * @returns {number}
+ */
+ var sumOfUnique = function(nums) {
+    let obj = {}, sum = 0;
+    for (ele of nums) {
+        obj[ele] = ++obj[ele] || 1;
+    }
+    console.log(obj)
+    
+    for(key in obj) {
+        if(obj[key] === 1) sum += Number(key);
+    }
+    
+    return sum;
+  };
+
+  var sumOfUnique1 = function(nums) {
+      let numMap = new Map(),
+      curr = 0;
+      nums.forEach((n) => {
+          numMap.set(n, numMap.get(n) + 1 || 1);
+      });
+      numMap.forEach((value, key) => {
+          if (value === 1) {
+              curr += key;
+          }
+      });
+
+      return curr;
+  }
+
+nums = [1,2,3,2]
+
+console.log(sumOfUnique(nums))
+nums1 = [1,2,3,2]
+
+console.log(sumOfUnique1(nums1))
