05dec3d5c9b0215844a1e9fed4a223cb7b8d81b9
Fajr <fajri.illahi@medicaboo.com>
2021-04-26 04:31:14 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/tempCodeRunnerFile.js
+++ 1-basic/tempCodeRunnerFile.js
@@ -1,33 +1,12 @@
+var repeatedNTimes = function(A) {
+  let numMap = new Map()
+  A.forEach((e,i,arr) => {
+      numMap.set(e, numMap.get(e) + 1 || 1)
+  })
+  let res = 0;
+  numMap.forEach((value, key) => {
+    if(value>1) {res = key; break;}
+  })
 
- // Definition for a binary tree node.
-  function TreeNode(val, left, right) {
-     this.val = (val===undefined ? 0 : val)
-     this.left = (left===undefined ? null : left)
-     this.right = (right===undefined ? null : right)
- }
- 
-/**
- * @param {TreeNode} root
- * @return {TreeNode}
- */
-
-
-
-var increasingBST = function(root) {
-   let result = []; // node value from in order
-
-   function inOrder(node) {
-     if(!node) return;
-     inOrder(node.left);
-     result.push(node.val);
-     inOrder(node.right)
-   }
-
-   inOrder(root);
-   console.log(result)
-    
+  return res;
 };
-
-root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
-console.log(new TreeNode(root))
-console.log(increasingBST(root))
