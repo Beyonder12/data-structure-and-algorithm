fa2738a63aa4265f35c354eb4e0a0abb021578a8
Fajr <fajri.illahi@medicaboo.com>
2021-04-26 13:30:47 +0700

    Update input2.js

--- 1-basic/input2.js
+++ 1-basic/input2.js
@@ -1,47 +1,29 @@
 /**
- * @param {number[]} arr
- * @return {number[]}
+ * @param {string} moves
+ * @return {boolean}
  */
- var replaceElements = function(arr) {
-  let a = [], arr1 = arr;
-
-  for(let i = 0; i < arr.length; i++) {
-      arr1.splice(i, 1, 0)
-      a.push(Math.max(...arr1))
-    
+ var judgeCircle = function(moves) {
+  let x = 0, y = 0, values = {L: -1, D: -1, R: 1, U: 1};
+  for(let i = 0; i < moves.length; i++) {
+      // console.log(moves[i] === 'L' || moves[i] === 'R' )
+      // console.log(values[moves[i]])
+      (moves[i] === 'L' || moves[i] === 'R') ? x += values[moves[i]] : y += values[moves[i]];
   }
-  a[arr.length - 1] = -1;
-  return a;
+
+  return (x===0 && y===0) ? true : false;
+  
 };
 
-const replaceElements = arr => {
-  const result = new Array(arr.length);
-  result[arr.length - 1] = -1;
-  
-  for (let i = arr.length - 1; i > 0; i -= 1) {
-      result[i - 1] = Math.max(arr[i], result[i]);
-  }
-  
-  return result;
-};
+moves = 'UD'
+moves1 = 'LL'
 
-function replaceElements(arr) {
-  let max = -1;
-  
-  for (let i = arr.length - 1; i >= 0; i--) {
-    const curr = arr[i];
-    arr[i] = max;
-    max = Math.max(max, curr);
-  }
-  
-  return arr;
-}
 
-res = [18, 6,  6, 6, 1, -1]
-arr = [17, 18, 5, 4, 6,  1]
+console.log(judgeCircle(moves))
+console.log(judgeCircle(moves1))
+console.log('====================')
+console.log('ok')
 
-// console.log(arr.push(-1))
 
-console.log(replaceElements(arr))
-// console.log(arr.push(-1))
-// console.log(arr)
+// Input: moves = "UD"
+// Output: true
+// Explanation: The robot moves up once, and then down once. All moves have the same magnitude, so it ended up at the origin where it started. Therefore, we return true.
