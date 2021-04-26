d0b706471a35d39b357f3aff38d5f4061e32e82e
Fajr <fajri.illahi@medicaboo.com>
2021-04-26 13:01:15 +0700

    Update input2.js

--- 1-basic/input2.js
+++ 1-basic/input2.js
@@ -1,14 +1,47 @@
-var repeatedNTimes = function(A) {
-  let numMap = new Map()
-  A.forEach((e,i,arr) => {
-      numMap.set(e, numMap.get(e) + 1 || 1)
-  })
-  let res = 0;
-  numMap.forEach((value, key) => {
-    if(value>1) {res = key; break;}
-  })
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+ var replaceElements = function(arr) {
+  let a = [], arr1 = arr;
 
-  return res;
+  for(let i = 0; i < arr.length; i++) {
+      arr1.splice(i, 1, 0)
+      a.push(Math.max(...arr1))
+    
+  }
+  a[arr.length - 1] = -1;
+  return a;
 };
 
-console.log(repeatedNTimes([5,1,5,2,5,3,5,4]))
+const replaceElements = arr => {
+  const result = new Array(arr.length);
+  result[arr.length - 1] = -1;
+  
+  for (let i = arr.length - 1; i > 0; i -= 1) {
+      result[i - 1] = Math.max(arr[i], result[i]);
+  }
+  
+  return result;
+};
+
+function replaceElements(arr) {
+  let max = -1;
+  
+  for (let i = arr.length - 1; i >= 0; i--) {
+    const curr = arr[i];
+    arr[i] = max;
+    max = Math.max(max, curr);
+  }
+  
+  return arr;
+}
+
+res = [18, 6,  6, 6, 1, -1]
+arr = [17, 18, 5, 4, 6,  1]
+
+// console.log(arr.push(-1))
+
+console.log(replaceElements(arr))
+// console.log(arr.push(-1))
+// console.log(arr)
