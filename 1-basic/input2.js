8b6d76f9f21d2e7fea26545ade702203aff9a64a
Fajr <fajri.illahi@medicaboo.com>
2021-04-26 19:48:28 +0700

    Update input2.js

--- 1-basic/input2.js
+++ 1-basic/input2.js
@@ -14,12 +14,34 @@
   
 };
 
+var judgeCircle1 = function(moves) {    
+  let x=0,y=0
+  for(i=0;i<moves.length;i++){
+      switch (moves[i]) {
+          case 'R':
+              x++;
+              break;
+          case 'L':
+              x--;
+              break;
+          case 'U':
+              y++;
+              break;
+          case 'D':
+              y--;
+              break;
+      }
+  }
+   return x === 0 && y === 0
+
+  
+};
 moves = 'UD'
 moves1 = 'LL'
 
 
 console.log(judgeCircle(moves))
-console.log(judgeCircle(moves1))
+console.log(judgeCircle1(moves1))
 console.log('====================')
 console.log('ok')
 
