c1c6476da45502c753ab2e5109316e3ceab8c3db
Fajr <fajri.illahi@medicaboo.com>
2021-04-27 01:00:49 +0700

    Update input2.js

--- 1-basic/input2.js
+++ 1-basic/input2.js
@@ -13,25 +13,29 @@ heights = [1,1,4,2,1,3]
   for (let i = 0; i < heights.length; i++) {
       if(heights[i] !== expected[i]) count++;
   }
-  console.log( expected)
-  console.log( heights)
+  // console.log( expected)
+  // console.log( heights)
   return count;
  
 };
 
 const heightChecker1 = heights => {
   const count = new Int8Array(101);
+  // console.log(count)
   for (let i = 0; i < heights.length; ++i) ++count[heights[i]];
+  
   let ret = idx = 0;
+  console.log(count.slice(0,5), heights)
   for (let i = 0; i < 101; ++i) {
+    
     while (count[i]--) heights[idx++] !== i && ++ret;
   }
   return ret;
 };
 
-console.log(heightChecker(heights))
+// console.log(heightChecker(heights))
 console.log(heightChecker1(heights))
 
-a = b = c = 100;
+for (let i = 0; i < 5;++i) console.log(i);
 
-console.log(a,b,c)
+// console.log(a,b,c)
