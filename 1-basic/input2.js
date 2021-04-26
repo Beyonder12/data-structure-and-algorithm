a0d47cd31bb0867d1d06289c745fcbe5d706fd7e
Fajr <fajri.illahi@medicaboo.com>
2021-04-27 00:17:56 +0700

    Update input2.js

--- 1-basic/input2.js
+++ 1-basic/input2.js
@@ -1,20 +1,37 @@
+heights = [1,1,4,2,1,3]
+
+
 /**
- * @param {string} s
- * @return {number[]}
+ * @param {number[]} heights
+ * @return {number}
  */
- const diStringMatch = (S) => {
-  let num = [];
-  let inc = 0;
-  let dec = S.length;
-  let i = 0;
-  while(num.length !== S.length+1){
-      num[i] = S[i] === 'D'? dec--: inc++;
-      i++;
+ var heightChecker = function(heights) {
+  let expected = [...heights], count = 0;
+  
+  expected.sort((a,b) => a - b);
+  
+  for (let i = 0; i < heights.length; i++) {
+      if(heights[i] !== expected[i]) count++;
   }
-  return num;
+  console.log( expected)
+  console.log( heights)
+  return count;
+ 
 };
 
-Input: s = "IDID"
-Output: [0,4,1,3,2]
+const heightChecker1 = heights => {
+  const count = new Int8Array(101);
+  for (let i = 0; i < heights.length; ++i) ++count[heights[i]];
+  let ret = idx = 0;
+  for (let i = 0; i < 101; ++i) {
+    while (count[i]--) heights[idx++] !== i && ++ret;
+  }
+  return ret;
+};
 
-console.log(diStringMatch(s))
+console.log(heightChecker(heights))
+console.log(heightChecker1(heights))
+
+a = b = c = 100;
+
+console.log(a,b,c)
