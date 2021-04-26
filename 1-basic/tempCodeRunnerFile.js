dcd931257e9384c5f991dd8918a3711834c700f2
Fajr <fajri.illahi@medicaboo.com>
2021-04-26 13:01:13 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/tempCodeRunnerFile.js
+++ 1-basic/tempCodeRunnerFile.js
@@ -1,12 +1,1 @@
-var repeatedNTimes = function(A) {
-  let numMap = new Map()
-  A.forEach((e,i,arr) => {
-      numMap.set(e, numMap.get(e) + 1 || 1)
-  })
-  let res = 0;
-  numMap.forEach((value, key) => {
-    if(value>1) {res = key; break;}
-  })
-
-  return res;
-};
+(-1))
