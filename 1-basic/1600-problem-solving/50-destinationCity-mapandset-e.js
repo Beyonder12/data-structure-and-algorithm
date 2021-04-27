02710d166d71c9b63dc2d92062e8327b9fb63337
Fajr <fajri.illahi@medicaboo.com>
2021-04-28 03:14:41 +0700

    Update 50-destinationCity-mapandset-e.js

--- 1-basic/1600-problem-solving/50-destinationCity-mapandset-e.js
+++ 1-basic/1600-problem-solving/50-destinationCity-mapandset-e.js
@@ -15,11 +15,11 @@
 
 const destCity = (paths) => {
     const map = new Map();
-    console.log(map)
+ 
     for (const path of paths) {
         map.set(path[0], map.has(path[0]) ? 0 : 1);
         map.set(path[1], map.has(path[1]) ? 0 : -1);
-        console.log(map)
+    
     }
     for (const item of map) {
         if (item[1] === -1) return item[0];
@@ -39,10 +39,10 @@ console.log(destCity(paths))
 
 const destCity1 = (paths) => {
     const set = new Set();
-    console.log(set)
+ 
     for (const path of paths) {set.add(path[0]); console.log(set);};
     for (const path of paths) {
-        console.log(path)
+        
         if (!set.has(path[1])) return path[1];
     }
 }
