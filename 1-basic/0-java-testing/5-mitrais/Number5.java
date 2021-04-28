872375a000a5d286121d8b34b18dd279564953c3
Fajr <fajri.illahi@medicaboo.com>
2021-04-28 22:16:59 +0700

    Create Number5.java

---
+++ 1-basic/0-java-testing/5-mitrais/Number5.java
@@ -0,0 +1,11 @@
+public class Number5 {
+    public static void main(String[] args) {
+        int x = 6;
+        while(x <= 9) {
+        System.out.print(x);
+        x++;
+        }
+    }
+}
+    
+    
