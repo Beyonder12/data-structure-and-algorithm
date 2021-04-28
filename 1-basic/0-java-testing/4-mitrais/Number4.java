8eb980083bb400a087ec9ab0b5d8e80bcc0ecf9f
Fajr <fajri.illahi@medicaboo.com>
2021-04-28 22:16:54 +0700

    Create Number4.java

---
+++ 1-basic/0-java-testing/4-mitrais/Number4.java
@@ -0,0 +1,11 @@
+public class Number4 {
+    public static void func(int x) {
+        x = 8;
+        System.out.println(x);
+    }
+    public static void main(String[] args) {
+        int x = 3;
+        func(x);
+        System.out.print(x);
+    }
+}
