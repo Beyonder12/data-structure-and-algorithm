01e6663428fefb17c22523ef65221bc189ffe039
Fajr <fajri.illahi@medicaboo.com>
2021-04-28 22:16:47 +0700

    Create Number3.java

---
+++ 1-basic/0-java-testing/3-mitrais/Number3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+
+    class Number3 {
+        static boolean sayHello() {
+            System.out.print("true");
+            return true;
+        }
+        public static void main(String[] args) {
+                if(sayHello()) {
+                    System.out.print(sayHello());
+                };
+            }
+    }
+    
+
