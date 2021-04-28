2da20247b4cdae425cdf5cfbf764fc0e79eec1b0
Fajr <fajri.illahi@medicaboo.com>
2021-04-28 22:17:04 +0700

    Create Number6.java

---
+++ 1-basic/0-java-testing/6-mitrais/Number6.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Number6 { 
+    public static void main(String[] args) {
+        int x = (int)(Math.random());
+        System.out.println(x);
+        int y =9/2;
+        System.out.println(y);
+    }
+}
