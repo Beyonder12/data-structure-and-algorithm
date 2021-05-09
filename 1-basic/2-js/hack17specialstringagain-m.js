db960117a569d1212a8bdebf92c3a2ac599306be
Fajr <fajri.illahi@medicaboo.com>
2021-05-10 00:37:42 +0700

    Update hack17specialstringagain-m.js

--- 1-basic/2-js/hack17specialstringagain-m.js
+++ 1-basic/2-js/hack17specialstringagain-m.js
@@ -18,3 +18,47 @@ function substrCount(n, s) {
 }
 
 console.log(substrCount(4,'aaaa'))
+
+function substrCount(n, s) {
+  let count = 0;
+  for (var i = 0; i < n - 1; i++) {
+      if (s[i] === s[i+1]) {
+          let adder = 1;
+          let j = i + 1;
+          while (j < n && s[i] === s[j]) {
+              count++;
+              adder++;
+              j++;
+          }
+          if (j + 1 < n - adder) {
+              if (s.substr(j+1, adder) === s.substr(i, adder)) {
+                  count++;
+              }
+          }
+      }
+      else if (s[i] === s[i+2]) count++;
+  }
+  return count + n;
+}
+
+
+function substrCount(n, s) {
+  let count = n;
+  for (let i = 0; i < s.length; i++) {
+      let nextIndex = i;
+      while (s[i] === s[nextIndex + 1]) nextIndex++;
+
+      if (i !== nextIndex) {
+          const length = nextIndex - i;
+          count = count + (length * (length + 1)) / 2;
+          i = nextIndex;
+      } else {
+          let step = 1;
+          while (s[i + step] === s[i - step] && s[i + step] === s[i + 1]) {
+              step++;
+              count++;
+          }
+      }
+  }
+  return count;
+}
