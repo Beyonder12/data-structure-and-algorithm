d6382744ac0625d1e484681ea2d0044d8adf916b
Fajr <fajri.illahi@medicaboo.com>
2021-05-09 17:27:58 +0700

    Create hack15stringdeletionangram-e.js

---
+++ 1-basic/2-js/hack15stringdeletionangram-e.js
@@ -0,0 +1,20 @@
+function makeAnagram(a, b) {
+  let map = {}, count = 0, tot = a.length + b.length;
+
+  for(let char of a) {
+      map[char] = (map[char] || 0) + 1;
+  }
+
+  for(let char of b) {
+      map[char] = (map[char] || 0) - 1;
+  }
+  console.log(map)
+  for (let key in map) {
+      count +=Math.abs(map[key])
+  }
+  return count;
+
+}
+a = 'cde'
+b = 'abc'
+console.log(makeAnagram(a, b))
