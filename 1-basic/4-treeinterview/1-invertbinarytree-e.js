7afe8d08f5fa25629cd9948fa46e1d01ab65b212
Fajr <fajri.illahi@medicaboo.com>
2021-05-11 01:38:06 +0700

    Create 1-invertbinarytree-e.js

---
+++ 1-basic/4-treeinterview/1-invertbinarytree-e.js
@@ -0,0 +1,34 @@
+//Definition for a binary tree node
+function TreeNode(val, left, right) {
+  this.val = (val ===undefined ? 0 : val)
+  this.left = (left ===undefined ? null : left)
+  this.right = (right===undefined ? null : right)
+}
+
+/**
+ *
+ * @param {TreeNode} root
+ * @returns {TreeNode}
+ */
+
+var invertTree = function(root) {
+  dfs(root);
+
+  return root;
+}
+
+function dfs(node) {
+
+  if(!node) {
+    return;
+  }
+  let temp = node.left;
+  node.left = node.right;
+  node.right = temp;
+
+  dfs(node.left);
+  dfs(node.right)
+
+}
+root = [4,2,7,1,3,6,9]
+console.log(invertTree(root))
