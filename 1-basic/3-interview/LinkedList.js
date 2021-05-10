611cbe6d3b9195e1372fb978595dc4b961015373
Fajr <fajri.illahi@medicaboo.com>
2021-05-11 01:38:18 +0700

    Create LinkedList.js

---
+++ 1-basic/3-interview/LinkedList.js
@@ -0,0 +1,46 @@
+class LinkedList {
+  constructor() {
+    this.head = null
+    this.length = 0
+  }
+  insertAtHead(data) {
+    const newNode = new LinkedListNode(data, this.head)
+    this.head = newNode
+    this.length++
+  }
+  getByIndex(index) {
+    if (index < 0 || index >= this.length) return null
+
+    let current = this.head
+    for (let i = 0; i < index; i++) {
+      current = current.next
+    }
+    return current
+  }
+  print() {
+    let output = ''
+    let current = this.head
+    while(current) {
+      output = `${output}${current.value} -> `
+      current = current.next
+    }
+    console.log(`${output}null`)
+  }
+}
+
+class LinkedListNode {
+  constructor(value, next) {
+    this.value = value
+    this.next = next
+  }
+}
+
+LinkedList.fromValues = function(...values) {
+  const ll = new LinkedList()
+  for (let i = values.length - 1; i >= 0; i--) {
+    ll.insertAtHead(values[i])
+  }
+  return ll;
+}
+
+module.exports = LinkedList
