f3535f6b6535ea6408c2e5f7956c279c5c1d7359
Fajr <fajri.illahi@medicaboo.com>
2021-05-12 00:32:15 +0700

    Update 71-arraytimessign-reducees6-e.js

---
+++ 1-basic/1600-problem-solving/71-arraytimessign-reducees6-e.js
@@ -0,0 +1,6 @@
+var arraySign = function(nums) {
+    return (nums.reduce((c,v) => c*v)) ? ((nums.reduce((c,v) => c*v) > 0) ? 1 : -1) : 0;
+  };
+  
+  nums = [-1,-2,-3,-4,3,2,1]
+  console.log(arraySign(nums))
