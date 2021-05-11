ee07ebac67d343d8f76998443974911eecec8191
Fajr <fajri.illahi@medicaboo.com>
2021-05-12 00:32:11 +0700

    Update app.js

--- 1-basic/3-interview/app.js
+++ 1-basic/3-interview/app.js
@@ -3,3 +3,7 @@ const LinkedList = require('./LinkedList')
 const ll = LinkedList.fromValues(10, 20, 30, 40)
 ll.print()
 
+for(let i = 0; i < 3; i++) {}
+  if(i=== 1) continue;
+  console.log(i);
+end
