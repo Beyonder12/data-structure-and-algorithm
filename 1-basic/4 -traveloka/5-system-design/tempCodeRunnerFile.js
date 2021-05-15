b05a212014300d3468219765b9ff5e94e703ba9b
Fajr <fajri.illahi@medicaboo.com>
2021-05-15 08:53:22 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/4 -traveloka/5-system-design/tempCodeRunnerFile.js
+++ 1-basic/4 -traveloka/5-system-design/tempCodeRunnerFile.js
@@ -1,1 +1,14 @@
-tweetId, time
+
+// for(let i = 0; i < 5; i++) {
+//   console.log("halo", "ke- ", i)
+// }
+
+umur = 30
+
+if(umur > 17 && umur < 50) {
+  console.log("Anda sudah dewasa!")
+} else if(umur <= 16) {
+  console.log("Anda Bocil")
+} else if(umur >= 50) {
+  console.log("Anda tua!")
+}
