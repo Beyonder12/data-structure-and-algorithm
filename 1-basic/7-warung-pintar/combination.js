29432623187a12543bd1bc82f0a2826100eebaaa
Fajr <fajri.illahi@medicaboo.com>
2021-05-18 09:39:27 +0700

    Update combination.js

--- 1-basic/7-warung-pintar/combination.js
+++ 1-basic/7-warung-pintar/combination.js
@@ -1,9 +1,5 @@
 
 
-
-
-
-
 function combinations(arr) {
   if(arr.length === 0) return [[]];
   let firstEl = arr[0], rest = arr.slice(1);
@@ -21,3 +17,28 @@ function combinations(arr) {
 
 let arr = [1, 2, 3]
 console.log(combinations(arr))
+//time complexity: O(2^n)
+//space complexity: O(2^n)
+
+
+function permutations(arr) {
+  if(arr.length === 0) return [[]];
+
+  const firstEl = arr[0];
+  const rest = arr.slice(1);
+
+  const permsWithoutFirst = permutations(rest);
+  const allPermutations = [];
+
+  permsWithoutFirst.forEach(perm => {
+    for (let i = 0; i <= perm.length; i++) {
+      const permWithFirst = [...perm.slice(0,i), firstEl, ...perm.slice(i)];
+      allPermutations.push(permWithFirst);
+    }
+  });
+
+  return allPermutations;
+
+}
+
+console.log(permutations(arr))
