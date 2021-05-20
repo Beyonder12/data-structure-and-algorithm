238ec39a6112e62f46cca47dd459519d1ae18680
Fajr <fajri.illahi@medicaboo.com>
2021-05-21 01:40:56 +0700

    Create 11-sumbst4-e.js

---
+++ 1-basic/4 -traveloka/4-treeinterview/11-sumbst4-e.js
@@ -0,0 +1,36 @@
+function TreeNode(val, left, right){
+  this.val = val ? val : null;
+  this.left = left ? left : null;
+  this.right = right ? right : null;
+}
+
+let root = new TreeNode(1);
+root.left = new TreeNode(2);;
+root.right = new TreeNode(3);;
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);;
+root.right.left = new TreeNode(6);;
+root.right.right = new TreeNode(7);;
+console.log(root)
+
+
+function findTarget(root, k) {
+
+  let set = new Set();
+  function go(node) {
+    if(!node) return false;
+    if(set.has(k - node.val)) return true;
+    set.add(node.val);
+    return go(node.left) || go(node.right)
+  }
+
+  return go(root)
+}
+let i = 30
+while(i) {
+  console.log(i, findTarget(root, i));
+  i--
+}
+
+
+
