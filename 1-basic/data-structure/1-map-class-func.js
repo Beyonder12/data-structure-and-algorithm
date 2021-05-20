0e61e96599ceebab15c13a49538ba18c4c5d6d3f
Fajr <fajri.illahi@medicaboo.com>
2021-05-20 17:02:41 +0700

    Update 1-map-class-func.js

--- 1-basic/data-structure/1-map-class-func.js
+++ 1-basic/data-structure/1-map-class-func.js
@@ -37,6 +37,12 @@ var m1 = new Map();
 m1.add("p1","Tom");
 m1.add("p2","Jerry");
 m1.add("p3", "Julie");
+console.log(m1)
 console.log(m1.values());
 console.log(m1.size());
 m1.clear();
+
+m2 = new Map();
+console.log(m2.add(1, 1))
+console.log(m2.add(1, 2))
+console.log(m2)
