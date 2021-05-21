ef680b101718a64e72ea9ca2251043a3e15742b3
Fajr <fajri.illahi@medicaboo.com>
2021-05-22 05:25:44 +0700

    Create 14-sameTree-recursion-e.js

---
+++ 1-basic/4 -traveloka/4-treeinterview/14-sameTree-recursion-e.js
@@ -0,0 +1,25 @@
+function TreeNode(val, left, right){
+  this.val = val ? val : 0;
+  this.left = left ? left : null;
+  this.right = right ? right : null;
+}
+
+let root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+root.right.left = new TreeNode(6);
+root.right.right = new TreeNode(7);
+console.log(root)
+
+let root1 = new TreeNode(1);
+root1.left = new TreeNode(2);
+root1.right = new TreeNode(3);
+root1.left.left = new TreeNode(4);
+root1.left.right = new TreeNode(5);
+root1.right.left = new TreeNode(6);
+root1.right.right = new TreeNode(7);
+console.log(root1)
+
+if(root && root1) return true
