7c6841edb10d59454705933d0c9b0f2e81944c80
Fajr <fajri.illahi@medicaboo.com>
2021-05-21 22:53:19 +0700

    Update 12-minsbsolutediff-helper-e.js

--- 1-basic/4 -traveloka/4-treeinterview/12-minsbsolutediff-helper-e.js
+++ 1-basic/4 -traveloka/4-treeinterview/12-minsbsolutediff-helper-e.js
@@ -14,18 +14,16 @@ root.right.right = new TreeNode(699);;
 console.log(root)
 
 
-var getMinimumDifference = function(root) {
+var getMinimumDifference1 = function(root) {
   let prevNodeVal = Number.MAX_VALUE;
   let min = Number.MAX_VALUE;
 
   // traverse Inorder to compare elements sequentially to find min
   function helper(node) {
-      if (node == null) {
-          return;
-      }
-
+      if (!node) return;
       helper(node.left);
       min = Math.min(min, Math.abs(node.val - prevNodeVal))
+
       prevNodeVal = node.val;
       helper(node.right);
   }
@@ -34,4 +32,28 @@ var getMinimumDifference = function(root) {
   return min;
 };
 
+var getMinimumDifference = function(root) {
+  let res = [];
+
+  function helper(node) {
+      if(!node) return;
+
+      res.push(node.val)
+      helper(node.left);
+      helper(node.right);
+  }
+  helper(root);
+  let min = Infinity;
+  // res is now an array of all value
+  for(let i = 0; i < res.length; i++) {
+    for(let j = i+1; j < res.length; j++) {
+      min = Math.min(min, Math.abs(res[i] - res[j]))
+    }
+  }
+
+  return min;
+}
+
+console.log(getMinimumDifference1(root))
 console.log(getMinimumDifference(root))
+
