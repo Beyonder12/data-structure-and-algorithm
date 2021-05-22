c16226c8e861f255be28f84d94e39c9500705ca6
Fajr <fajri.illahi@medicaboo.com>
2021-05-23 04:49:03 +0700

    Update 1-invertbinarytree-e.js

--- 1-basic/4 -traveloka/4-treeinterview/1-invertbinarytree-e.js
+++ 1-basic/4 -traveloka/4-treeinterview/1-invertbinarytree-e.js
@@ -29,6 +29,21 @@ function dfs(node) {
   dfs(node.left);
   dfs(node.right)
 
+  return root
 }
 root = [4,2,7,1,3,6,9]
 console.log(invertTree(root))
+
+
+function inversionString(arr) {
+  let i = 0, j = arr.length - 1;
+  while(i< j) {
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    i++; j--;
+  }
+  return arr;
+}
+
+console.log(inversionString([1,2,3,4,5]))
