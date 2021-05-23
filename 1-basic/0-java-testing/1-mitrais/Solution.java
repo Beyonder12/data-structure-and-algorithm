190066fb680d1f39b450293de2ad0352a24b6577
Fajr <fajri.illahi@medicaboo.com>
2021-05-23 21:02:02 +0700

    Update Solution.java

--- 1-basic/0-java-testing/1-mitrais/Solution.java
+++ 1-basic/0-java-testing/1-mitrais/Solution.java
@@ -1,8 +1,42 @@
-import java.util.*;
+// Java program for recursive implementation
+// of Bubble sort
 
-public class Solution {
-    static {
-      System.out.println("(0.0%0.0)");
-      System.exit(0);
+import java.util.Arrays;
+
+public class Solution
+{
+    // A function to implement bubble sort
+    static void bubbleSort(int arr[], int n)
+    {
+        // Base case
+        if (n == 1)
+            return;
+
+        // One pass of bubble sort. After
+        // this pass, the largest element
+        // is moved (or bubbled) to end.
+        for (int i=0; i<n-1; i++)
+            if (arr[i] > arr[i+1])
+            {
+                // swap arr[i], arr[i+1]
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+
+        // Largest element is fixed,
+        // recur for remaining array
+        bubbleSort(arr, n-1);
+    }
+
+    // Driver Method
+    public static void main(String[] args)
+    {
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+
+        bubbleSort(arr, arr.length);
+
+        System.out.println("Sorted array : ");
+        System.out.println(Arrays.toString(arr));
     }
 }
