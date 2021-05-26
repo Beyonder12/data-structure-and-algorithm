1b0cf3b8c09ca1bfe9672f9f1287d5e88d6ffadc
Fajr <fajri.illahi@medicaboo.com>
2021-05-26 22:12:51 +0700

    Create binary.js

---
+++ 1-basic/000-a-FFP/binary.js
@@ -0,0 +1,11 @@
+// program to convert decimal to binary
+
+// take input
+// const number = parseInt(prompt('Enter a decimal number: '));
+
+// convert to binary
+let number = 2.5
+const result = number.toString(2);
+
+console.log('Binary:' + ' ' + result);
+console.log(10.1 | 0)
