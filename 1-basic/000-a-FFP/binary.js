c801d621e4efb0c73c45ebdb826e53b46ec2af66
Fajr <fajri.illahi@medicaboo.com>
2021-05-29 20:22:35 +0700

    Update binary.js

--- 1-basic/000-a-FFP/binary.js
+++ 1-basic/000-a-FFP/binary.js
@@ -9,3 +9,31 @@ const result = number.toString(2);
 
 console.log('Binary:' + ' ' + result);
 console.log(10.1 | 0)
+
+
+
+
+function example() {
+
+}
+
+a = ['1', '2', '3', '4', '5'];
+console.log(Number(a))
+obj ={}
+
+keys =   [1,1,3,4,5,1,2,30]
+values = [1,3,4,5,6,8,9,10]
+com = keys + values;
+console.log(com)
+// console.log(obj[0])
+// obj[keys[0]] = [values[0]]
+// obj[keys[1]].push(values[1])
+// console.log(obj)
+// console.log(obj[1])
+console.log(5 >> 2)
+
+target = [4]
+
+while(--target[0]) {
+    console.log('push', 'pop')
+}
