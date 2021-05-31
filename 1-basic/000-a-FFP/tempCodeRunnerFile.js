bc2e0449645627cfab0410c20d65beca1a6718e2
Fajr <fajri.illahi@medicaboo.com>
2021-05-31 22:14:53 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/000-a-FFP/tempCodeRunnerFile.js
+++ 1-basic/000-a-FFP/tempCodeRunnerFile.js
@@ -1,8 +1,1 @@
-root = new TreeNode(1);
-root.left = new TreeNode(2);
-root.right = new TreeNode(3);
-root.left.left = new TreeNode(4)
-root.left.right = new TreeNode(5);
-root.right.left = new TreeNode(6);
-root.right.right = new TreeNode(7);
-console.log(root)
+nums.length
