73c9830a1da0b77c736aa9078a6402202244f6bf
Fajr <fajri.illahi@medicaboo.com>
2021-06-05 07:19:25 +0700

    Create Test.java

---
+++ java/multipleinheritance/Test.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+  // First Parent class
+  class Parent1
+  {
+      void fun()
+      {
+          System.out.println("Parent1");
+      }
+  }
+
+  // Second Parent Class
+  class Parent2
+  {
+      void fun()
+      {
+          System.out.println("Parent2");
+      }
+  }
+
+  // Error : Test is inheriting from multiple
+  // classes
+  class Test extends Parent1, Parent2
+  {
+    public static void main(String args[])
+    {
+        Test t = new Test();
+        t.fun();
+    }
+  }
+
