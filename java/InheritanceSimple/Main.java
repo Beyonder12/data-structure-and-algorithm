1cbe3a64443872a6a1d2416cd8600c10bfe8d059
Fajr <fajri.illahi@medicaboo.com>
2021-06-07 20:39:37 +0700

    Create Main.java

---
+++ java/InheritanceSimple/Main.java
@@ -0,0 +1,16 @@
+// import java.util.*;
+// import java.io.*;
+
+class Main {
+  public static void main(String[] args) {
+    // Persegi persegi = new Persegi();
+    // System.out.println("hello");
+    Persegi bangunDatar = new Persegi();
+    bangunDatar.luas(2);
+    bangunDatar.keliling(2);
+    System.out.println(bangunDatar.getLuas());
+    System.out.println(bangunDatar.getKeliling());
+    System.out.println(bangunDatar.sisi);
+
+  }
+}
