e1b7318920bb83f9404e6450832f81574a41463a
Fajr <fajri.illahi@medicaboo.com>
2021-06-13 23:03:55 +0700

    Update throwbasic.js

--- 1-basic/throwbasic.js
+++ 1-basic/throwbasic.js
@@ -1,11 +1,17 @@
-function isPositive(a) {
+var makeEqual = function(words) {
+  let hash = {};
+  if (words.length === 1) return true;
+  for(w of words) for(c of w) hash[c] = (hash[c] + 1) || 1;
+  console.log(hash)
+  for(h in hash) if(hash[h] % words.length !== 0) return false;
 
-if(a > 0){
-        return 'YES';
-    }
-    else{
-        throw (a === 0 ? new Error('Zero Error') : new Error('Negative Error'));
-    }
-}
+  return true;
+  // console.log(hash)
+};
+words = ["abc","aabc","bc"]
+words1 = ["abbab"]
+words2 = ["ab","a"]
 
-console.log(new Date('helo'))
+console.log(words, makeEqual(words))
+console.log(words1, makeEqual(words1))
+console.log(words2, makeEqual(words2))
