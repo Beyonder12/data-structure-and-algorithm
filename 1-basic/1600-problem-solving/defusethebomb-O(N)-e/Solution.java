5a1b1ea974eff6197161d07bc440f3a3097510d5
Fajr <fajri.illahi@medicaboo.com>
2021-06-19 12:58:54 +0700

    Create Solution.java

---
+++ 1-basic/1600-problem-solving/defusethebomb-O(N)-e/Solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+  public int[] decrypt(int[] code, int k) {
+    if(k == 0) return new int[code.length];
+
+    int[] prefix = new int[code.length * 2 + 1];
+    for(int i = 0; i < code.length * 2; i++) {
+      prefix[i+1] = prefix[i] + code[i % code.length];
+    }
+
+    int[] ret = new int[code.length];
+    for(int i = 0; i < code.length; i++) {
+      if end = i + k;
+      if(k < 0) {
+        ret[i] = prefix[i + code.length] - prefix[end + code.length];
+      }
+      else {
+        ret[i] = prefix[end + 1] - prefix[i+1];
+      }
+    }
+    return ret;
+  }
+}
