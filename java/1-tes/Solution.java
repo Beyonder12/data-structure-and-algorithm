614796361824dadc118f942750da169b082237f0
Fajr <fajri.illahi@medicaboo.com>
2021-06-19 12:58:25 +0700

    Create Solution.java

---
+++ java/1-tes/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+  public static void main(String[] args) {
+    System.out.println("hello");
+    // String S = "abcde";
+    // int N = S.length();
+    // int[] ans = new int[N];
+    // int prev = Integer.MIN_VALUE;
+    // System.out.println(N);
+    // System.out.println(Arrays.toString(ans));
+    // System.out.print(prev);
+  }
+}
