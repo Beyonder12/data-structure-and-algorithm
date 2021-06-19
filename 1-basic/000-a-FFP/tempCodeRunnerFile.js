92e684996d7100038eb3a0fb293ad4b1c483bc01
Fajr <fajri.illahi@medicaboo.com>
2021-06-19 12:58:58 +0700

    Update tempCodeRunnerFile.js

--- 1-basic/000-a-FFP/tempCodeRunnerFile.js
+++ 1-basic/000-a-FFP/tempCodeRunnerFile.js
@@ -1,1 +1,3 @@
-nums.length
+ // for(let i = 0; i < input.length; i++) {
+  //   if(input[i] !== 'a' || input[i] !== 'i' || input[i] !== 'u' || input[i] !== 'e' || input[i] !== 'o') ans.splice(i,1)
+  // }
