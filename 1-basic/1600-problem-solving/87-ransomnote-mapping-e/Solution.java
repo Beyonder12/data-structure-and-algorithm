b63ebd773c387d4d77766c1e7beff3028627ac0c
Fajr <fajri.illahi@medicaboo.com>
2021-07-01 17:20:09 +0700

    Create Solution.java

---
+++ 1-basic/1600-problem-solving/87-ransomnote-mapping-e/Solution.java
@@ -0,0 +1,21 @@
+
+
+public class Solution {
+  public static void main(String[] args) {
+    System.out.println(canConstuct("aa" , "aabb"));//true
+    System.out.println(canConstuct("tru", "rtt"));//false
+  }
+
+  public static boolean canConstuct(String ransomNote, String magazine) {
+    int[] arr = new int[26];
+    for (int i = 0; i < magazine.length(); i++) {
+      arr[magazine.charAt(i) - 'a']++;
+    }
+    for (int i = 0; i < ransomNote.length(); i++) {
+      if(--arr[ransomNote.charAt(i) - 'a'] < 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
