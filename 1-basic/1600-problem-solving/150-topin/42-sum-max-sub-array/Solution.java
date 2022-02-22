d1823e89ad01d77c26db5f371d4718a652a8770d
fajri.illahi <fajri.illahi@homecredit.co.id>
2022-02-22 23:52:22 +0700

    max sub array

--- 1-basic/1600-problem-solving/150-topin/42-sum-max-sub-array/Solution.java
+++ 1-basic/1600-problem-solving/150-topin/42-sum-max-sub-array/Solution.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
-    
+    public static int maxSubArray(int[] nums) {
+        int n = nums.length, max = Integer.MIN_VALUE, sum = 0;
+
+        for(int i = 0; i < n; i++) {
+            sum += nums[i];
+            max = Math.max(sum, max);
+            if(sum < 0) sum = 0;
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>();
+        nums.add(-2);
+        nums.add(1);
+        nums.add(-3);
+        nums.add(4);
+        nums.add(-1);
+        nums.add(2);
+        nums.add(1);
+        nums.add(-5);
+        nums.add(4);
+        System.out.println(maxSubArray(new int[] {-2,1,-3,4,-1,2,1,-5,4}));
+        System.out.println(maxSubArray(nums));
+    }
 }
